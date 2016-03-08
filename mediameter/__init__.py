6a808791ba7f8e3ed11726e6c36d627431d49d25
Rahul Bhargava <rahulb@media.mit.edu>
2016-03-08 12:12:56 -0500

    init server with connection to mc

--- mediameter/__init__.py
+++ mediameter/__init__.py
@@ -1,9 +1,34 @@
+import os, logging, ConfigParser
 from flask import Flask, render_template
 from flask_webpack import Webpack
 
+import mediacloud
+
 webpack = Webpack()
 
+# Set up some logging
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+logging.basicConfig(filename=os.path.join(base_dir,'logs','server.log'),level=logging.DEBUG)
+log = logging.getLogger(__name__)
+log.info("---------------------------------------------------------------------------")
+requests_logger = logging.getLogger('requests')
+requests_logger.setLevel(logging.WARN)
+mediacloud_logger = logging.getLogger('mediacloud')
+mediacloud_logger.setLevel(logging.DEBUG)
+
+# load the shared settings file
+server_config_file_path = os.path.join(base_dir,'server.config')
+settings = ConfigParser.ConfigParser()
+settings.read(server_config_file_path)
+
+# Connect to MediaCloud
+mc = mediacloud.api.AdminMediaCloud(settings.get('mediacloud','api_key'))
+log.info("Connection to mediacloud")
+
 def create_app():
+    '''
+    Factory method to create the app
+    '''
     app = Flask(__name__)
     settings = {
         'DEBUG': True,
