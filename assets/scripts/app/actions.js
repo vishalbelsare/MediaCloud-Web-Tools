7939a58f3a3639dc7c735396245914f73cab847f
Rahul Bhargava <rahulb@media.mit.edu>
2016-03-11 12:30:24 -0500

    comments

--- assets/scripts/app/actions.js
+++ assets/scripts/app/actions.js
@@ -2,6 +2,7 @@ import { createAction } from 'redux-actions';
 
 export const LEFT_NAV_VISIBILITY = 'LEFT_NAV_VISIBILITY';
 
+// takes a boolean argument indicating if it is open or not
 let leftNavVisibility = createAction(LEFT_NAV_VISIBILITY);
 
 export default leftNavVisibility;
--- assets/scripts/user/actions.js
+++ assets/scripts/user/actions.js
@@ -3,5 +3,8 @@ import { createAction } from 'redux-actions';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
+// takes one object arg - the user properties to store
 export const login = createAction(LOGIN);
+
+// no args
 export const logout = createAction(LOGOUT);
