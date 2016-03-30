d29a6016579277fc4d1557e55153c32bf85db03b
Rahul Bhargava <rahulb@media.mit.edu>
2016-03-29 21:36:04 -0400

    add controversy summary endpoint

--- mediameter/views/controversies.py
+++ mediameter/views/controversies.py
@@ -6,8 +6,17 @@ from mediameter import app, mc
 
 logger = logging.getLogger(__name__)
 
-@app.route('/api/controversy-list', methods=['GET'])
-@flask_login.login_required
+@app.route('/api/controversy/list', methods=['GET'])
+#@flask_login.login_required
 def api_controversy_list():
     controversy_list = mc.controversyList()
     return jsonify({'results':controversy_list})
+
+@app.route('/api/controversy/<controversy_id>/summary', methods=['GET'])
+#@flask_login.login_required
+def api_controversy_summary(controversy_id):
+	controversy = mc.controversy(controversy_id)
+	dumps = mc.controversyDumpList(controversy_id)
+	controversy['dumps'] = dumps
+	# TODO: iterate over list to flag the latest one (dump_date)
+	return jsonify({'results':controversy})
