d824d98e3fd9849bc7c8c8296e4035a904ae75f5
Rahul Bhargava <rahulb@media.mit.edu>
2016-05-11 10:28:35 -0400

    show top stores (fake data for now)

--- assets/scripts/components/controversy/summary/ControversyTopStories.js
+++ assets/scripts/components/controversy/summary/ControversyTopStories.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import Paper from 'material-ui/Paper';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 
 const ControversyTopStories = (props) => {
   const { stories } = props;
   return (
     <Paper>
-    Stories 
+      <Table selectable={false}>
+        <TableHeader adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Title</TableHeaderColumn>
+            <TableHeaderColumn>Media Source</TableHeaderColumn>
+            <TableHeaderColumn>Date</TableHeaderColumn>
+            <TableHeaderColumn>Inlinks</TableHeaderColumn>
+            <TableHeaderColumn>Outlinks</TableHeaderColumn>
+            <TableHeaderColumn>Clicks</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {stories.map(story =>
+            (<TableRow key={story.stories_id}>
+              <TableRowColumn><a href={story.url}>{story.title}</a></TableRowColumn>
+              // TODO: link this to the media source page with all selected filters applied
+              <TableRowColumn><a href={story.media_url}>{story.media_name}</a></TableRowColumn>
+              <TableRowColumn>{story.publish_date}</TableRowColumn>
+              <TableRowColumn>{story.inlinks}</TableRowColumn>
+              <TableRowColumn>{story.outlinks}</TableRowColumn>
+              <TableRowColumn>{story.bitly_click_count}</TableRowColumn>
+            </TableRow>)
+          )}
+        </TableBody>
+      </Table>
     </Paper>
   );
 };
