731925c28d71f9d532f73704ecfab4d2850cba5b
Rahul Bhargava <rahulb@media.mit.edu>
2016-05-20 10:04:47 -0400

    new (faked out) sentence count endpoint - need to replace with topic/sentencecount once it is available

--- mediameter/views/topics.py
+++ mediameter/views/topics.py
@@ -62,3 +62,26 @@ def api_topics_top_words(topic_id):
     timespan_id = request.args.get('timespan')
     media = mc.topicWordCount(topic_id,snapshot_id=snapshot_id,timespan_id=timespan_id)[:100]
     return jsonify({'results':media})
+
+@app.route('/api/topics/<topic_id>/sentences/count', methods=['GET'])
+#@flask_login.login_required
+def api_topics_sentence_count(topic_id):
+    snapshot_id = request.args.get('snapshot')
+    timespan_id = request.args.get('timespan')
+    # fake out for now
+    topicTimespan = mc.controversyDumpTimeSlice(timespan_id)
+    response = mc.sentenceCount("{~ controversy_dump_time_slice:"+str(timespan_id)+" }",
+        split=True,split_start_date=topicTimespan['start_date'][:10],
+        split_end_date=topicTimespan['end_date'][:10])
+    counts = []
+    for k,v in response['split'].iteritems():
+        if k in ['end','gap','start']:
+            continue
+        counts.append({ 'date': k, 'count':v })
+    results = {
+        'results':{
+            'total': response['count'],
+            'counts': counts
+        }
+    }
+    return jsonify(results)
