91ff9966eebb2fc587bc7bf74aa864300b843408
Rahul Bhargava <rahulb@media.mit.edu>
2016-06-01 13:43:26 -0400

    work on auth consistency

--- server/auth.py
+++ server/auth.py
@@ -13,12 +13,10 @@ class User(flask_login.UserMixin):
         self.id = key
         self.active = active
         self.created = datetime.datetime.now()
+        self.is_anonymous = False
         
     def is_active(self):
         return self.active
-    
-    def is_anonymous(self):
-        return False
     
     def is_authenticated(self):
         return True
