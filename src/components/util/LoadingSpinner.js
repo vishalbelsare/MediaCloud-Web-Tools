0cbfd54e115db330a25d60c807c61875df29a025
Rahul Bhargava <rahulb@media.mit.edu>
2016-06-15 10:00:12 -0400

    add optional padding and size props to loading spinner

--- src/components/util/LoadingSpinner.js
+++ src/components/util/LoadingSpinner.js
@@ -3,22 +3,31 @@ import CircularProgress from 'material-ui/CircularProgress';
 
 class LoadingSpinner extends React.Component {
   getStyles() {
+    const { padding } = this.props;
+    const paddingToUse = (padding !== undefined) ? padding : 10;
     const styles = {
       root: {
         textAlign: 'center',
-        padding: 10,
+        padding: paddingToUse,
       },
     };
     return styles;
   }
   render() {
+    const { size } = this.props;
+    const sizeToUse = (size !== undefined) ? (size / 50) : 0.5;
     const styles = this.getStyles();
     return (
       <div style={styles.root}>
-        <CircularProgress size={0.5} />
+        <CircularProgress size={sizeToUse} />
       </div>
     );
   }
 }
+
+LoadingSpinner.propTypes = {
+  padding: React.PropTypes.number,
+  size: React.PropTypes.number, // in pixels
+};
 
 export default LoadingSpinner;
