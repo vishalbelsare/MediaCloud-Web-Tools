eab69223e13cbcc9bd905be14079aad0d052eab9
Rahul Bhargava <rahulb@media.mit.edu>
2016-07-14 13:56:12 -0400

    refactor to use composeAsyncWidget

--- src/components/topic/stories/InfluentialStoriesContainer.js
+++ src/components/topic/stories/InfluentialStoriesContainer.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import ErrorTryAgain from '../../common/ErrorTryAgain';
-import LoadingSpinner from '../../common/LoadingSpinner';
 import InfluentialStories from './InfluentialStories';
 import { fetchTopicInfluentialStories, sortTopicInfluentialStories } from '../../../actions/topicActions';
-import * as fetchConstants from '../../../lib/fetchConstants.js';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import FlatButton from 'material-ui/FlatButton';
 import messages from '../../../resources/messages';
 import DownloadButton from '../../common/DownloadButton';
+import DataCard from '../../common/DataCard';
+import composeAsyncWidget from '../../util/composeAsyncWidget';
 
 const localMessages = {
   title: { id: 'topic.influentialStories.title', defaultMessage: 'Influential Stories' },
 };
 
 class InfluentialStoriesContainer extends React.Component {
-  componentDidMount() {
-    const { fetchStatus } = this.props;
-    if (fetchStatus !== fetchConstants.FETCH_FAILED) {
-      this.refetchData();
-    }
-  }
   componentWillReceiveProps(nextProps) {
     if ((nextProps.filters !== this.props.filters) ||
         (nextProps.sort !== this.props.sort)) {
       const { fetchData } = this.props;
-      const { topicId } = this.props.params;
       if ((nextProps.filters.snapshotId !== null) && (nextProps.filters.timespanId !== null)) {
-        fetchData(topicId, nextProps.filters.snapshotId, nextProps.filters.timespanId, nextProps.sort);
+        fetchData(nextProps);
       }
     }
   }
@@ -36,28 +28,13 @@ class InfluentialStoriesContainer extends React.Component {
     const { sortData } = this.props;
     sortData(newSort);
   }
-  getStyles() {
-    const styles = {
-      contentWrapper: {
-      },
-      iconStyles: {
-        marginRight: 24,
-      },
-      actionButtons: {
-        float: 'right',
-      },
-    };
-    return styles;
-  }
   refetchData = () => {
-    const { fetchData, filters, sort } = this.props;
-    const { topicId } = this.props.params;
-    fetchData(topicId, filters.snapshotId, filters.timespanId, sort);
+    const { fetchData } = this.props;
+    fetchData(this.props);
   }
   nextPage = () => {
-    const { fetchData, filters, sort, continuationId } = this.props;
-    const { topicId } = this.props.params;
-    fetchData(topicId, filters.snapshotId, filters.timespanId, sort, continuationId);
+    const { fetchData } = this.props;
+    fetchData(this.props);
   }
   downloadCsv = () => {
     const { filters, sort } = this.props;
@@ -66,38 +43,22 @@ class InfluentialStoriesContainer extends React.Component {
     window.location = url;
   }
   render() {
-    const { fetchStatus, fetchData, stories, sort } = this.props;
+    const { stories, sort } = this.props;
     const { topicId } = this.props.params;
     const { formatMessage } = this.props.intl;
-    let content = fetchStatus;
-    const styles = this.getStyles();
-    let headerContent = null;
-    switch (fetchStatus) {
-      case fetchConstants.FETCH_SUCCEEDED:
-        headerContent = <DownloadButton tooltip={formatMessage(messages.download)} onClick={this.downloadCsv} />;
-        content = (
-          <div>
-            <InfluentialStories topicId={topicId} stories={stories} onChangeSort={this.onChangeSort} sortedBy={sort} />
-            <FlatButton label={formatMessage(messages.nextPage)} primary onClick={this.nextPage} />
-          </div>
-        );
-        break;
-      case fetchConstants.FETCH_FAILED:
-        content = <ErrorTryAgain onTryAgain={fetchData} />;
-        break;
-      default:
-        content = <LoadingSpinner />;
-    }
     return (
-      <div style={styles.root}>
+      <div>
         <Grid>
           <Row>
             <Col lg={12} md={12} sm={12}>
-              <div style={styles.actionButtons}>
-                {headerContent}
-              </div>
-              <h2><FormattedMessage {...localMessages.title} /></h2>
-              {content}
+              <DataCard>
+                <div className="actions">
+                  <DownloadButton tooltip={formatMessage(messages.download)} onClick={this.downloadCsv} />
+                </div>
+                <h2><FormattedMessage {...localMessages.title} /></h2>
+                <InfluentialStories topicId={topicId} stories={stories} onChangeSort={this.onChangeSort} sortedBy={sort} />
+                <FlatButton label={formatMessage(messages.nextPage)} primary onClick={this.nextPage} />
+              </DataCard>
             </Col>
           </Row>
         </Grid>
@@ -123,6 +84,7 @@ InfluentialStoriesContainer.propTypes = {
   topicInfo: React.PropTypes.object.isRequired,
   filters: React.PropTypes.object.isRequired,
   continuationId: React.PropTypes.number,
+  topicId: React.PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -132,21 +94,33 @@ const mapStateToProps = (state) => ({
   continuationId: state.topics.selected.stories.continuation_id,
   filters: state.topics.selected.filters,
   topicInfo: state.topics.selected.info,
+  topicId: state.topics.selected.id,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchData: (topicId, snapshotId, timespanId, sort, continuationId) => {
-    if ((snapshotId !== null) && (timespanId !== null)) {
-      dispatch(fetchTopicInfluentialStories(topicId, snapshotId, timespanId, sort,
-        InfluentialStoriesContainer.ROWS_PER_PAGE, continuationId));
-    }
+  fetchData: (props) => {
+    dispatch(fetchTopicInfluentialStories(props.topicId, props.filters.snapshotId,
+      props.filters.timespanId, props.sort, InfluentialStoriesContainer.ROWS_PER_PAGE,
+      props.continuationId));
   },
   sortData: (sort) => {
     dispatch(sortTopicInfluentialStories(sort));
   },
 });
 
-export default injectIntl(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(InfluentialStoriesContainer));
+function mergeProps(stateProps, dispatchProps, ownProps) {
+  return Object.assign({}, stateProps, dispatchProps, ownProps, {
+    asyncFetch: () => {
+      dispatchProps.fetchData(stateProps);
+    },
+  });
+}
+
+export default
+  injectIntl(
+    connect(mapStateToProps, mapDispatchToProps, mergeProps)(
+      composeAsyncWidget(
+        InfluentialStoriesContainer
+      )
+    )
+  );
