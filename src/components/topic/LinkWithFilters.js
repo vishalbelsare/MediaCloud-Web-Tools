1101cfaf41a33dcb8a669660f784601a51ff261c
Rahul Bhargava <rahulb@media.mit.edu>
2016-07-21 08:26:19 -0400

    make component a function

--- src/components/topic/LinkWithFilters.js
+++ src/components/topic/LinkWithFilters.js
@@ -3,30 +3,22 @@ import { connect } from 'react-redux';
 import Link from 'react-router/lib/Link';
 
 /**
- * Use to include filters in a topic-related link by default.  Treat this as a drop-in 
+ * Use to include filters in a topic-related link by default.  Treat this as a drop-in
  * replacement for the react-router Link tag.
  **/
-class LinkWithFilters extends React.Component {
-  getStyles() {
-    const styles = {
-    };
-    return styles;
-  }
-  render() {
-    const { to, style, children, filters } = this.props;
-    const styles = this.getStyles();
-    const linkLocation = {
-      pathname: to,
-      query: {
-        snapshotId: filters.snapshotId,
-        timespanId: filters.timespanId,
-      },
-    };
-    return (
-      <Link to={linkLocation} style={style}>{children}</Link>
-    );
-  }
-}
+const LinkWithFilters = (props) => {
+  const { to, style, children, filters } = props;
+  const linkLocation = {
+    pathname: to,
+    query: {
+      snapshotId: filters.snapshotId,
+      timespanId: filters.timespanId,
+    },
+  };
+  return (
+    <Link to={linkLocation} style={style}>{children}</Link>
+  );
+};
 
 LinkWithFilters.propTypes = {
   // from parent
