93bf656be02693737096107cf52649b41631f1d2
Rahul Bhargava <rahulb@media.mit.edu>
2016-08-16 12:45:32 -0400

    more errortryagain styles to scss #27

--- src/components/common/ErrorTryAgain.js
+++ src/components/common/ErrorTryAgain.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import { grey400 } from 'material-ui/styles/colors';
 import { FormattedMessage, injectIntl } from 'react-intl';
 
 const localMessages = {
@@ -8,33 +7,17 @@ const localMessages = {
   errorTryAgain: { id: 'error.tryAgain', defaultMessage: 'Try Again' },
 };
 
-class ErrorTryAgain extends React.Component {
-  getStyles() {
-    const { padding } = this.props;
-    const styles = {
-      root: {
-        textAlign: 'center',
-        padding: (padding !== null) ? padding : 10,
-      },
-      p: {
-        color: grey400,
-      },
-    };
-    return styles;
-  }
-  render() {
-    const { onTryAgain } = this.props;
-    const { formatMessage } = this.props.intl;
-    const tryAgainMsg = formatMessage(localMessages.errorTryAgain);
-    const styles = this.getStyles();
-    return (
-      <div style={styles.root}>
-        <p style={styles.p}><FormattedMessage {...localMessages.errorCantLoad} /></p>
-        <FlatButton label={tryAgainMsg} primary onClick={onTryAgain} />
-      </div>
-    );
-  }
-}
+const ErrorTryAgain = (props) => {
+  const { onTryAgain } = props;
+  const { formatMessage } = props.intl;
+  const tryAgainMsg = formatMessage(localMessages.errorTryAgain);
+  return (
+    <div className="error-try-again">
+      <p><FormattedMessage {...localMessages.errorCantLoad} /></p>
+      <FlatButton label={tryAgainMsg} primary onClick={onTryAgain} />
+    </div>
+  );
+};
 
 ErrorTryAgain.propTypes = {
   intl: React.PropTypes.object.isRequired,
