420e3e13319accb704b9de24b7e516c18bb9a85e
Rahul Bhargava <rahulb@media.mit.edu>
2016-08-16 14:05:38 -0400

    lint cleanup

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,14 @@
-import os, logging, ConfigParser, logging.config, json, sys
+import os
+import logging
+import logging.config
+import ConfigParser
+import json
+import sys
 from flask import Flask, render_template
 from flask_webpack import Webpack
-import pymongo, flask_login, mediacloud
+import pymongo
+import flask_login
+import mediacloud
 from flask_cors import CORS
 
 SERVER_MODE_DEV = "DEV"
@@ -10,9 +17,9 @@ SERVER_MODE_PROD = "PROD"
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Set up some logging
-with open(os.path.join(base_dir,'config','server-logging.json'), 'r') as f:
+with open(os.path.join(base_dir, 'config', 'server-logging.json'), 'r') as f:
     logging_config = json.load(f)
-    logging_config['handlers']['file']['filename'] = os.path.join(base_dir,logging_config['handlers']['file']['filename'])
+    logging_config['handlers']['file']['filename'] = os.path.join(base_dir, logging_config['handlers']['file']['filename'])
 logging.config.dictConfig(logging_config)
 logger = logging.getLogger(__name__)
 logger.info("---------------------------------------------------------------------------")
@@ -20,26 +27,26 @@ flask_login_logger = logging.getLogger('flask_login')
 flask_login_logger.setLevel(logging.DEBUG)
 
 # load the shared settings file
-server_config_file_path = os.path.join(base_dir,'config','server.config')
+server_config_file_path = os.path.join(base_dir, 'config', 'server.config')
 settings = ConfigParser.ConfigParser()
 settings.read(server_config_file_path)
 
-server_mode = settings.get('server','mode')
+server_mode = settings.get('server', 'mode')
 if server_mode not in [SERVER_MODE_DEV, SERVER_MODE_PROD]:
-    logger.error("Unknown server mode '%s', set a mode in the `config/server.config` file" % (server_mode))
+    logger.error("Unknown server mode '%s', set a mode in the `config/server.config` file", server_mode)
     sys.exit(1)
 else:
-    logger.info("Started server in %s mode" % (server_mode))
+    logger.info("Started server in %s mode", server_mode)
 
 # Connect to MediaCloud
-mc = mediacloud.api.AdminMediaCloud(settings.get('mediacloud','api_key'))
+mc = mediacloud.api.AdminMediaCloud(settings.get('mediacloud', 'api_key'))
 logger.info("Connected to mediacloud")
 
 # Connect to the app's mongo DB
 db_host = settings.get('database', 'host')
 db_name = settings.get('database', 'name')
 db = pymongo.MongoClient(db_host)[db_name]
-logger.info("Connected to DB: %s@%s" % (db_name,db_host))
+logger.info("Connected to DB: %s@%s", db_name, db_host)
 
 def isDevMode():
     return server_mode == SERVER_MODE_DEV
@@ -53,17 +60,17 @@ def create_app():
     '''
     Factory method to create the app
     '''
-    app = Flask(__name__)
+    my_app = Flask(__name__)
     webpack_config = {
         'DEBUG': isDevMode(),
         'WEBPACK_MANIFEST_PATH': '../build/manifest.json' if isDevMode() else '../server/static/gen/manifest.json'
     }
-    app.config.update(webpack_config)
-    webpack.init_app(app)
-    return app
+    my_app.config.update(webpack_config)
+    webpack.init_app(my_app)
+    return my_app
 
 app = create_app()
-app.secret_key = settings.get('server','secret_key')
+app.secret_key = settings.get('server', 'secret_key')
 
 CORS(app,
     resources=r'/*',
@@ -82,12 +89,12 @@ def index():
     logger.debug("homepage request")
     return render_template('index.html')
 
-import views.login
-import views.sources
-import views.topics.media
-import views.topics.sentences
-import views.topics.stories
-import views.topics.topic
-import views.topics.words
-import views.topics.focalsets
-import views.topics.foci
+import server.views.login
+import server.views.sources
+import server.views.topics.media
+import server.views.topics.sentences
+import server.views.topics.stories
+import server.views.topics.topic
+import server.views.topics.words
+import server.views.topics.focalsets
+import server.views.topics.foci
--- server/views/util/csv.py
+++ server/views/util/csv.py
@@ -1,9 +1,10 @@
 import logging
-import datetime, flask
+import datetime
+import flask
 
 logger = logging.getLogger(__name__)
 
-def stream_response(data,dict_keys,filename,column_names=None):
+def stream_response(data, dict_keys, filename, column_names=None):
     """Stream a fully ready dict to the user as a csv.
     Keyword arguments:
     data -- an array of dicts
@@ -17,7 +18,7 @@ def stream_response(data,dict_keys,filename,column_names=None):
     logger.debug("  cols: "+' '.join(column_names))
     logger.debug("  props: "+' '.join(dict_keys))
     # stream back a csv
-    def stream_as_csv(dataset,props,names):
+    def stream_as_csv(dataset, props, names):
         yield ','.join(names) + '\n'
         for row in dataset:
             try:
@@ -25,12 +26,12 @@ def stream_response(data,dict_keys,filename,column_names=None):
                 for p in props:
                     value = row[p]
                     cleaned_value = value
-                    if isinstance( value, ( int, long, float ) ):
+                    if isinstance(value, (int, long, float)):
                         cleaned_value = str(row[p])
                     elif value is None:
                         cleaned_value = ""
                     else:
-                        cleaned_value = '"'+value.encode('utf-8').replace('"','""')+'"'
+                        cleaned_value = '"'+value.encode('utf-8').replace('"', '""')+'"'
                     attributes.append(cleaned_value)
                 #attributes = [ csv_escape(str(row[p])) for p in props]
                 yield ','.join(attributes) + '\n'
@@ -39,5 +40,5 @@ def stream_response(data,dict_keys,filename,column_names=None):
                 logger.exception(e)
                 logger.debug(row)
     download_filename = 'mediacloud-'+str(filename)+'-'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')+'.csv'
-    return flask.Response(stream_as_csv(data,dict_keys,column_names), mimetype='text/csv; charset=utf-8', 
+    return flask.Response(stream_as_csv(data, dict_keys, column_names), mimetype='text/csv; charset=utf-8',
                 headers={"Content-Disposition":"attachment;filename="+download_filename})
