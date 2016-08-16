cb8a7bf50ff7ae7f41b1225af05f893b2e3be955
Rahul Bhargava <rahulb@media.mit.edu>
2016-08-16 12:50:49 -0400

    move loading-spinner styles to scss (#27)

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
-class LoadingSpinner extends React.Component {
-  getStyles() {
-    const { padding } = this.props;
-    const paddingToUse = (padding !== undefined) ? padding : 10;
-    const styles = {
-      root: {
-        textAlign: 'center',
-        padding: paddingToUse,
-      },
-    };
-    return styles;
-  }
-  render() {
-    const { size } = this.props;
-    const sizeToUse = (size !== undefined) ? (size / 50) : 0.5;
-    const styles = this.getStyles();
-    return (
-      <div style={styles.root}>
-        <CircularProgress size={sizeToUse} />
-      </div>
-    );
-  }
-}
+const LoadingSpinner = (props) => {
+  const { size, padding } = props;
+  const sizeToUse = (size !== undefined) ? (size / 50) : 0.5;
+  const customStyle = (padding !== undefined) ? { padding } : null;
+  return (
+    <div className="loading-spinner" style={customStyle}>
+      <CircularProgress size={sizeToUse} />
+    </div>
+  );
+};
 
 LoadingSpinner.propTypes = {
   padding: React.PropTypes.number,
