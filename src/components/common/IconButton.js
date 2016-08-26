65a13a14c874cbe2bb46611d277a427973d95890
Rahul Bhargava <rahulb@media.mit.edu>
2016-08-26 09:36:41 -0400

    fix Safari double-tap bug

--- src/components/common/IconButton.js
+++ src/components/common/IconButton.js
@@ -26,7 +26,7 @@ function composeIconButton(Icon, tooltipMessage) {
       const linkTarget = linkTo || formatMessage(tooltipMessage);
       const clickHandler = (onClick) ? this.handleClick : null;
       return (
-        <Link to={linkTarget} onClick={clickHandler} className="icon-button-link" name={formatMessage(tooltipMessage)}>
+        <Link to={linkTarget} onTouchTap={clickHandler} className="icon-button-link" name={formatMessage(tooltipMessage)}>
           <IconButton
             tooltip={formatMessage(tooltipMessage)}
             style={{ padding: 0, border: 0, width: 26, height: 26 }}
