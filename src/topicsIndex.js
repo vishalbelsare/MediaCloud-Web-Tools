e185797d5578802f4e810f8760bf480660f5f7b9
Rahul Bhargava <rahulb@media.mit.edu>
2016-09-07 13:49:01 -0400

    fix hash parsing error

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -5,6 +5,10 @@ import store from './store';
 import initializeApp from './index.js';
 import { filterBySnapshot, filterByTimespan, filterByFocus } from './actions/topicActions';
 
+/**
+ * This serves as the primary entry point to the Media Cloud Topic Mapper app.
+ */
+
 setAppName('topics');
 
 setAppColors({
@@ -16,19 +20,21 @@ setAppColors({
 const hash = window.location.hash;
 const hashParts = hash.split('?');
 const args = {};
-const queryParts = hashParts[1].split('&');
-queryParts.forEach((part) => {
-  const argParts = part.split('=');
-  args[argParts[0]] = argParts[1];
-});
-if ('snapshotId' in args) {
-  store.dispatch(filterBySnapshot(args.snapshotId));
-}
-if ('timespanId' in args) {
-  store.dispatch(filterByTimespan(args.timespanId));
-}
-if ('focusId' in args) {
-  store.dispatch(filterByFocus(args.focusId));
+if (hashParts.length > 1) {
+  const queryParts = hashParts[1].split('&');
+  queryParts.forEach((part) => {
+    const argParts = part.split('=');
+    args[argParts[0]] = argParts[1];
+  });
+  if ('snapshotId' in args) {
+    store.dispatch(filterBySnapshot(args.snapshotId));
+  }
+  if ('timespanId' in args) {
+    store.dispatch(filterByTimespan(args.timespanId));
+  }
+  if ('focusId' in args) {
+    store.dispatch(filterByFocus(args.focusId));
+  }
 }
 
 initializeApp(routes);
