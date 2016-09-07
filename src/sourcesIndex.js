9a3231d1f03cfa5aa6520efc596786b1e917b06c
Rahul Bhargava <rahulb@media.mit.edu>
2016-09-07 13:48:48 -0400

    comment

--- src/sourcesIndex.js
+++ src/sourcesIndex.js
@@ -3,6 +3,10 @@ import { setAppColors } from './styles/colors.js';
 import routes from './routes/sourceRoutes';
 import initializeApp from './index.js';
 
+/**
+ * This serves as the primary entry point to the Media Cloud Source Manager app.
+ */
+
 setAppName('sources');
 
 setAppColors({
