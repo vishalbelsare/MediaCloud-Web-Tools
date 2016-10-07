609e352429d18a1311327bff274dfd5da7eecf8b
Rahul Bhargava <rahulb@media.mit.edu>
2016-10-06 21:17:07 -0400

    style link to snapshot builder on needs-snapshot warning

--- src/components/topic/NeedsNewSnapshotWarning.js
+++ src/components/topic/NeedsNewSnapshotWarning.js
@@ -2,44 +2,40 @@ import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import FontIcon from 'material-ui/FontIcon';
-import RaisedButton from 'material-ui/RaisedButton';
-import messages from '../../resources/messages';
+import Link from 'react-router/lib/Link';
 
 const localMessages = {
   warning: { id: 'needSnapshot.warning', defaultMessage: 'You\'ve made changes to your Topic that require a new snapshot to be generated!' },
+  snapshotBuilderLink: { id: 'needSnapshot.snapshotBuilderLink', defaultMessage: 'Visit the Snapshot Builder for details.' },
 };
 
-class NeedsNewSnapshotWarning extends React.Component {
-
-  render() {
-    const { needsNewSnapshot, onGenerateSnapshotRequest } = this.props;
-    const { formatMessage } = this.props.intl;
-    if (needsNewSnapshot === false) {
-      return null;
-    }
-    return (
-      <div className="needs-new-snapshot-warning">
-        <Grid>
-          <Row>
-            <Col lg={10} md={10} sm={6}>
-              <FontIcon className="material-icons" color={'#000000'}>warning</FontIcon>
-              <FormattedMessage {...localMessages.warning} />
-            </Col>
-            <Col lg={2} md={2} sm={6}>
-              <RaisedButton label={formatMessage(messages.snapshotGenerate)} onClick={onGenerateSnapshotRequest} />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    );
+const NeedsNewSnapshotWarning = (props) => {
+  const { needsNewSnapshot } = props;
+  if (needsNewSnapshot === false) {
+    return null;
   }
-
-}
+  return (
+    <div className="needs-new-snapshot-warning">
+      <Grid>
+        <Row>
+          <Col lg={12}>
+            <FontIcon className="material-icons" color={'#000000'}>warning</FontIcon>
+            <FormattedMessage {...localMessages.warning} />
+            &nbsp;
+            <Link to={`/topics/${props.topicId}/snapshot`}>
+              <FormattedMessage {...localMessages.snapshotBuilderLink} />
+            </Link>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+};
 
 NeedsNewSnapshotWarning.propTypes = {
   intl: React.PropTypes.object.isRequired,
   needsNewSnapshot: React.PropTypes.bool.isRequired,
-  onGenerateSnapshotRequest: React.PropTypes.func.isRequired,
+  topicId: React.PropTypes.number.isRequired,
 };
 
 export default injectIntl(NeedsNewSnapshotWarning);
