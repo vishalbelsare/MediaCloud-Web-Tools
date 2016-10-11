f8fd10e2036f960cb0039ea0002c69db24fbabbb
Rahul Bhargava <rahulb@media.mit.edu>
2016-10-11 10:52:07 -0400

    wired in permissions endpoints

--- server/views/topics/permissions.py
+++ server/views/topics/permissions.py
@@ -4,33 +4,17 @@ import flask_login
 
 from server import app
 from server.util.request import form_fields_required, json_error_response
+from server.auth import user_mediacloud_client
 
 logger = logging.getLogger(__name__)
 
-# TODO: put in real API method once it is ready
 @app.route('/api/topics/<topics_id>/permissions/list', methods=['GET'])
 @flask_login.login_required
 def topic_permissions_list(topics_id):
-    return jsonify(
-        {
-            "permissions":
-            [
-                {
-                    "email": "hroberts@cyber.law.harvard.edu",
-                    "topics_id": topics_id,
-                    "permission": "admin"
-                },
-                {
-                    "email": "foo@foo.bar",
-                    "topics_id": topics_id,
-                    "permission": "read"
-                }
+    user_mc = user_mediacloud_client()
+    results = user_mc.topicPermissionsList(topics_id)
+    return jsonify(results)
 
-            ]
-        }
-    )
-
-# TODO: put in real API method once it is ready
 @app.route('/api/topics/<topics_id>/permissions/update', methods=['PUT'])
 @form_fields_required('email', 'permission')
 @flask_login.login_required
@@ -39,12 +23,6 @@ def topic_update_permission(topics_id):
     permission = request.form["permission"]
     if permission not in ['read', 'write', 'admin', 'none']:
         return json_error_response('Invalid permission value')
-    return jsonify({
-        "permissions": [
-            {
-                "email": email,
-                "topics_id": topics_id,
-                "permission": permission
-            }
-        ]
-    })
+    user_mc = user_mediacloud_client()
+    results = user_mc.topicPermissionsUpdate(topics_id, email, permission)
+    return jsonify(results)
