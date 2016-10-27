f641aa298d67f563906f4452d09a3c53770c0da2
cindy bishop <cindyloo_home@yahoo.com>
2016-10-27 15:32:53 -0400

    more work on endpoints

--- server/__init__.py
+++ server/__init__.py
@@ -104,16 +104,16 @@ def index():
     logger.debug("homepage request")
     return render_template('index.html')
 
-# now load in the appropriate view endpoints
+# now load in the appropriate view endpoints (only on Prod)
 import server.views.user
 server_app = settings.get('server', 'app')
-if (server_app == SERVER_APP_SOURCES) or isProdMode():
+if (server_app == SERVER_APP_SOURCES) or isDevMode():
     import server.views.sources.collection
     import server.views.sources.source
     import server.views.sources.sentences
     import server.views.sources.words
     import server.views.sources.geocount
-if (server_app == SERVER_APP_TOPICS) or isProdMode():
+if (server_app == SERVER_APP_TOPICS) or isDevMode():
     import server.views.topics.media
     import server.views.topics.sentences
     import server.views.topics.stories
