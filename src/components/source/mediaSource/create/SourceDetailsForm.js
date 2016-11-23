f762d93faa06e77e22b6bcf46889b97ef3718ed5
Rahul Bhargava <rahulb@media.mit.edu>
2016-11-23 16:44:42 -0500

    labels on source details form component

--- src/components/source/mediaSource/create/SourceDetailsForm.js
+++ src/components/source/mediaSource/create/SourceDetailsForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { Row, Col } from 'react-flexbox-grid/lib';
 import composeIntlForm from '../../../common/IntlForm';
 
 const localMessages = {
   nameLabel: { id: 'source.add.name.label', defaultMessage: 'Name of Source' },
   urlLabel: { id: 'source.add.url.label', defaultMessage: 'URL' },
-  notesLabel: { id: 'source.add.notes.label', defaultMessage: 'Editor`s Notes' },
+  notesLabel: { id: 'source.add.notes.label', defaultMessage: 'Editor\'s Notes' },
   nameError: { id: 'source.add.name.error', defaultMessage: 'You have to enter a name for this source.' },
   urlError: { id: 'source.add.url.error', defaultMessage: 'Pick have to enter a url for this source.' },
 };
@@ -17,30 +17,42 @@ const SourceDetailsForm = (props) => {
   return (
     <div className="source-details-form">
       <Row>
-        <Col lg={12}>
+        <Col md={2}>
+          <span className="label unlabeled-field-label">
+            <FormattedMessage {...localMessages.nameLabel} />
+          </span>
+        </Col>
+        <Col md={4}>
           <Field
             name="name"
             component={renderTextField}
-            floatingLabelText={localMessages.nameLabel}
           />
         </Col>
       </Row>
       <Row>
-        <Col lg={12}>
+        <Col md={2}>
+          <span className="label unlabeled-field-label">
+            <FormattedMessage {...localMessages.urlLabel} />
+          </span>
+        </Col>
+        <Col md={4}>
           <Field
             name="url"
             component={renderTextField}
-            floatingLabelText={localMessages.urlLabel}
           />
         </Col>
       </Row>
       <Row>
-        <Col lg={12}>
+        <Col md={2}>
+          <span className="label unlabeled-field-label">
+            <FormattedMessage {...localMessages.notesLabel} />
+          </span>
+        </Col>
+        <Col md={8}>
           <Field
             name="notes"
             component={renderTextField}
             fullWidth
-            floatingLabelText={localMessages.notesLabel}
           />
         </Col>
       </Row>
