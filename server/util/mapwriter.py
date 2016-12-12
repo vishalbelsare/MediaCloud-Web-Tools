21305178e00be1d5e62a9f589a99017419c47bfe
Rahul Bhargava <rahulb@media.mit.edu>
2016-12-12 12:19:26 -0500

    integrate Sands wordMap code to generate them

---
+++ server/util/mapwriter.py
@@ -0,0 +1,205 @@
+from networkx.readwrite import json_graph
+import networkx as nx
+import community
+import pandas as pd
+import numpy as np
+import datetime
+import requests
+import mediacloud
+import json
+import os
+import copy
+import logging
+
+from server import mc, base_dir
+
+logger = logging.getLogger(__name__)
+
+def build_top_words(media_list, topics_id, timespans_id, remove_words=None, word_count=100):
+    # TODO: Add parameters for wordCount resolution (sample size, etc.)
+    # Output word count run statistics to file for metadata / paradata record
+    top_words = {}
+    if(remove_words == None):
+        remove_words=[]
+    media_sources = {ms['media_id']: ms['name'] for ms in media_list}
+    for m in [ms['media_id'] for ms in media_list]:
+        top_words[m] = mc.topicWordCount(
+            topics_id,
+            timespans_id=timespans_id, 
+            q="media_id:%s" % m, 
+            num_words=word_count+len(remove_words)
+        )
+        logger.debug('Got %d words from %s' % (len(top_words[m]), media_sources[m]))
+    
+    logger.debug('\nAll Words Complete!')
+    return top_words
+
+def clean_top_words(word_set, remove_words_list, word_limit=100):
+    for media_source in word_set:
+        # remove stopwords        
+        word_set[media_source] = [w for w in word_set[media_source] if w['term'] not in remove_words_list]
+
+        # trim any extra words above the word limit
+        word_set[media_source] = word_set[media_source][:word_limit]
+        
+        # maybe output some summary data
+        return word_set
+
+def build_network(top_words, sources, media_attribs=None):
+    source_ids = [ms['media_id'] for ms in sources]
+    media_sources = {ms['media_id']: ms['name'] for ms in sources}
+    
+    # Media Source / Word Network
+    msw_network = nx.DiGraph()
+    # msw_network = nx.Graph()
+
+    pairwise_sources = {}
+
+    for m in source_ids:
+        # pairwise_sources[m] = nx.DiGraph()
+        
+        if(m in top_words.keys()):
+            for w in top_words[m]:
+                msw_network.add_node(w['term'], type='word',
+                    viz = {'color':{'r':77,'g':7,'b':0}, 
+                        'position':{'x':1.0,'y':1.0,'z':0.0}, 
+                    'size':42}
+                )
+                if(media_attribs == None):
+                    msw_network.add_node(media_sources[m], type='media_source')
+                else:
+                    msw_network.add_node(media_sources[m], type='media_source', 
+                        category=media_attribs[m])
+                        
+                msw_network.add_edge(media_sources[m], w['term'], weight=w['count'])
+
+                # pairwise_sources[m].add_node(w['term'], type='word')
+                # pairwise_sources[m].add_node(media_sources[m], type='media_source')
+                # pairwise_sources[m].add_edge(media_sources[m], w['term'], weight=w['count'])
+        else:
+            logger.debug('Skipping %s...' % media_sources[m])
+
+    # for node in msw_network.nodes_iter():
+    #     msw_network[node]['viz'] = {}
+    #     msw_network[node]['viz']['position'] = {}
+    #     msw_network[node]['viz']['position']['x'] = "1.0"
+    #     msw_network[node]['viz']['position']['y'] = "1.0"
+    #     msw_network[node]['viz']['position']['z'] = "0.0"
+    #     msw_network[node]['viz']['color'] = {}
+    #     msw_network[node]['viz']['color']['r'] = "100"
+    #     msw_network[node]['viz']['color']['g'] = "100"
+    #     msw_network[node]['viz']['color']['b'] = "100"
+    #     msw_network[node]['viz']['size'] = "10"
+
+    # logger.debug "== COMMUNITY DETECTION ==\n\n"
+    # partition = community.best_partition(msw_network)
+    # count = 0.
+    # for com in set(partition.values()):
+    #     count = count + 1.
+    #     list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
+    #     for node in list_nodes:
+    #         logger.debug "- {0}\n".format(node)
+    #         n = msw_network[node]
+    #         n['modularity_class'] = str(count)
+
+    return msw_network
+
+def export_gexf_network(network, filename):
+    logger.debug('Writing %d nodes to network in %s.' % (network.number_of_nodes(), filename))
+    nx.write_gexf(network, filename)
+    
+def export_d3_network(network, filename):
+    logger.debug('Writing %d nodes to network as D3 JSON graph.' % (network.number_of_nodes()))
+    data = json_graph.node_link_data(network)
+    with open(filename + '.json', 'w') as f:
+        f.write(json.dumps(data))
+    
+def get_top_x_sources(topics_id, source_count, timespans_id):
+    top = mc.topicMediaList(topics_id, timespans_id=timespans_id, limit=source_count)['media']
+    return top
+                
+# Remove Media Source from Network 
+def remove_word_source_from_network(ms_name, word_list):
+    ms = mc.mediaList(name_like=ms_name)
+    if(len(ms) == 1):
+        try:
+            del word_list[ms[0]['media_id']]
+        except KeyError:
+            logger.debug('Media Source not present in list.')
+    elif(len(ms) == 0):
+        logger.debug('No match for %s.' % ms_name)
+    else:
+        logger.debug('Multiple matches for Media Source. No action taken.')
+        
+# Remove Media Source from Media Source List
+def remove_media_source(remove_name, media_sources):
+    for idx, ms in enumerate(media_sources):
+        if(ms['name'] == remove_name):
+            logger.debug('Deleting %s (%d)' % (ms['name'], ms['media_id']))
+            del media_sources[idx]
+            return media_sources
+    logger.debug('Media Source %s Not Present.' % remove_name)
+    return media_sources
+
+# TODO: Enable media source range based on significance, and allow selection of social or inlink, 
+def generate_network_of_frames(topics_id, timespans_id, num_of_sources, out_name=None, remove_media_list=None, remove_word_list=[], generate_word_lists=False, include_media_list=None, media_attribs=None, num_words=None):
+    if(out_name == None):
+        out_name = 'network_of_frames-%s' % datetime.datetime.now().isoformat()
+    
+    if(remove_media_list == None):
+        remove_media_list = []
+
+# use this specify attributes on the media source that should be added to the node as attributes        
+#     if(media_attribs == None):
+#         media_attribs = {}
+        
+    if(include_media_list == None):
+        media_sources_md = get_top_x_sources(topics_id, num_of_sources+len(remove_media_list), timespans_id)
+    else:
+        media_sources_md = include_media_list
+
+    if(remove_media_list != None):
+        for r in remove_media_list:
+            media_sources_md = remove_media_source(r, media_sources_md)
+
+    top_words = build_top_words(media_sources_md, topics_id, timespans_id, remove_word_list, num_words)
+    if(remove_word_list != None):
+        top_words = clean_top_words(top_words, remove_word_list)
+
+    frame_network = build_network(top_words, media_sources_md, media_attribs)
+
+    export_gexf_network(frame_network, '%s.gexf' % out_name)
+    export_d3_network(frame_network, '%s' % out_name)
+    
+    if(generate_word_lists == True):
+        with open('%s-word-usage.txt' % out_name, 'wb') as wl:
+            all_words = []
+            media_sources = {ms['media_id']: ms['name'] for ms in media_sources_md}
+            counts = {}
+            for ms in top_words:
+                wl.write("\n\n%s (media id: %d):\n" % (media_sources[ms].encode('ascii', 'ignore'), ms))
+                for w in top_words[ms]:
+                    all_words.append(w['term'])
+
+                    # increment count to see how many media source include each word
+                    # counts[ms]
+
+                    wl.write("- %s (%d)\n" % (w['term'].encode('ascii', 'ignore'), w['count']))
+                wl.write("\n")
+    
+    linefeed=chr(10) # linefeed=\n
+    s=linefeed.join(nx.generate_gexf(frame_network))  # doctest: +SKIP
+    # for line in nx.generate_gexf(frame_network):  # doctest: +SKIP
+    #     logger.debug line
+
+    return s
+
+def create_word_map_files(topics_id, timespans_id, filepath):
+    # Defaults
+    num_of_sources = 50
+    # remove_media_sources = ['digg.com', 'delicious.com', 'en-gb.facebook', 'newsvine.com', 'nationalservice.gov', 'nobelprize.org', 'Twitter', 'YouTube', 'selmamovie.com', 'hollywoodreporter.com', 'variety.com', 'imdb.com']
+    remove_media_sources = []
+    remove_words = []
+    nof = generate_network_of_frames(topics_id, timespans_id, num_of_sources, filepath, 
+        remove_media_sources, remove_words, generate_word_lists=True, num_words=100)
+    return out_file
--- server/views/topics/maps.py
+++ server/views/topics/maps.py
@@ -7,6 +7,7 @@ from server import app, base_dir
 import server.util.csv as csv
 from server.auth import user_mediacloud_key
 from server.util.request import arguments_required, filters_from_args, api_error_handler
+from server.util import mapwriter
 
 DATA_DIR = os.path.join(base_dir, "data", "map-files")
 MAP_TYPES = ['wordMap', 'linkMap']
@@ -18,23 +19,28 @@ logger = logging.getLogger(__name__)
 @arguments_required('snapshotId','timespanId')
 @api_error_handler
 def map_files(topics_id):
-    files = {}
+    files = { 
+        'wordMap': 'unsupported',
+        'linkMap': 'not rendered'
+    }
     snapshots_id, timespans_id, foci_id = filters_from_args(request.args)
-    for map_type in MAP_TYPES:
-        status = None
-        prefix = _get_file_prefix(map_type,topics_id,snapshots_id,foci_id,timespans_id)
-        lock_filename = prefix+".lock"
-        rendered_filename = prefix+".gefx"
-        # check if rendered file is there
-        is_rendered = os.path.isfile(os.path.join(DATA_DIR,rendered_filename))
-        if is_rendered:
-            status = 'rendered'
-        else:
-            is_generating = os.path.isfile(os.path.join(DATA_DIR,lock_filename))
-            if not is_generating:
-                _start_generating_map_file(map_type,topics_id,snapshots_id, foci_id, timespans_id)
-            status = 'generating'
-        files[map_type] = status
+    map_type = MAP_TYPES[0] # no linkMaps yet
+    status = None
+    prefix = _get_file_prefix(map_type, topics_id, timespans_id)
+    lock_filename = prefix+".lock"
+    rendered_filename = prefix+".gexf"
+    # check if rendered file is there
+    is_rendered = os.path.isfile(os.path.join(DATA_DIR,rendered_filename))
+    logger.warn(os.path.join(DATA_DIR,rendered_filename))
+    logger.warn(is_rendered)
+    if is_rendered:
+        status = 'rendered'
+    else:
+        is_generating = os.path.isfile(os.path.join(DATA_DIR,lock_filename))
+        if not is_generating:
+            _start_generating_map_file(map_type, topics_id, timespans_id)
+        status = 'generating'
+    files[map_type] = status
     return jsonify(files)
 
 '''
@@ -51,9 +57,16 @@ def request_map_file(topics_id, map_type):
     return jsonify({'success': 1})
 '''
 
-def _start_generating_map_file(map_type, topics_id, snapshots_id, foci_id, timespans_id):
-    lock_filename = _get_file_prefix(map_type,topics_id,snapshots_id, foci_id, timespans_id)+'.lock'
-    open(os.path.join(DATA_DIR,lock_filename), 'a').close()        
+def _start_generating_map_file(map_type, topics_id, timespans_id):
+    file_prefix = _get_file_prefix(map_type, topics_id, timespans_id)
+    # create map file
+    lock_filename = file_prefix+'.lock'
+    open(os.path.join(DATA_DIR,lock_filename), 'a').close()
+    # generate maps
+    file_path = os.path.join(DATA_DIR, file_prefix)
+    mapwriter.create_word_map_files(topics_id, timespans_id, file_path)
+    # remove lock file
+    os.remove(lock_filename)
 
-def _get_file_prefix(map_type, topics_id, snapshots_id, foci_id, timespans_id):
-    return map_type+"-"+str(topics_id)+'-'+str(snapshots_id)+'-'+str(foci_id)+'-'+str(timespans_id)
+def _get_file_prefix(map_type, topics_id, timespans_id):
+    return map_type+"-"+str(topics_id)+"-"+str(timespans_id)
