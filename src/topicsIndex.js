fdfeed32eab13cb685664d3bd4ecdbed9149ea89
Rahul Bhargava <rahulb@rbots-mbp.media.mit.edu>
2017-01-04 14:09:16 -0500

    topics release prep

--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -7,7 +7,7 @@ import initializeApp from './index';
  * This serves as the primary entry point to the Media Cloud Topic Mapper app.
  */
 
-setVersion('1.1.3');
+setVersion('1.1.4');
 
 setAppName('topics');
 
