44491fd63db03cf44fa0c901e1d7fd6a884f2e2b
Rebekah Bell <bellr@mit.edu>
2017-02-02 15:14:21 -0500

    login acceptance test

--- test/acceptance/login.test.js
+++ test/acceptance/login.test.js
@@ -1,9 +1,22 @@
+import config from '../../config/test.config';
 import visit from '../helpers/visit';
 
-describe('When visiting the homepage', () => {
-  test('it welcomes the user', async function () {
-    let page = visit('/');
-    let text = await page.evaluate(() => document.body.textContent).end();
-    expect(text).toContain('Welcome to React');
+describe('When visiting the log in page', () => {
+
+	beforeAll(function(done) {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL=30000;
+		done();
+	});
+
+  test('it logs user in', async() => {
+  	let result = await visit('/#/login')
+  					  .type('.login-form input[type=text]', config.username)
+  					  .type('.login-form input[type=password]', config.password)
+  					  .click('.login-form .app-button button')
+  					  .wait('#content h1 span')
+  					  .evaluate(() => document.querySelector('#content h1 span').innerText)
+  					  .end();
+
+  	expect(result).toBe('Media Cloud Sources and Collections');
   });
 });
