af19667b6a195539b5aefa7383d194c3ba2e40a2
Rahul B <rahulbot@gmail.com>
2017-02-04 21:11:20 -0500

    default timespan selector to open (fix #233)

--- src/reducers/topics/selected/timespans.js
+++ src/reducers/topics/selected/timespans.js
@@ -22,7 +22,7 @@ function getTimespanFromListById(list, id) {
 
 const initialState = {
   list: [],
-  isVisible: false,
+  isVisible: true,
   selectedPeriod: 'overall',
   selectedId: null, // annoying that I have to keep this here too... topic.filters should be the one true source of this info :-(
   selected: null,
