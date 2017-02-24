7de8679a85b65dc1d29b5df12e9186a05f3e666e
Rahul B <rahulbot@gmail.com>
2017-02-24 14:12:24 -0500

    build for release and version bump

--- src/sourcesIndex.js
+++ src/sourcesIndex.js
@@ -7,7 +7,7 @@ import initializeApp from './index';
  * This serves as the primary entry point to the Media Cloud Source Manager app.
  */
 
-setVersion('1.3.2');
+setVersion('1.3.3');
 
 setAppName('sources');
 
