6306fb8a59f1d904e0e8f083233db5d5e15da14d
Rahul B <rahulbot@gmail.com>
2017-03-01 10:52:55 -0500

    add buttons to browse collections to sources homepage (#275)

--- src/components/source/homepage/Homepage.js
+++ src/components/source/homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { injectIntl, FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
 import Link from 'react-router/lib/Link';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import FeaturedCollectionsContainer from './FeaturedCollectionsContainer';
 import PopularCollectionsContainer from './PopularCollectionsContainer';
 import FavoriteSourcesAndCollectionsContainer from './FavoriteSourcesAndCollectionsContainer';
+import AppButton from '../../common/AppButton';
 import DataCard from '../../common/DataCard';
 import LoginForm from '../../user/LoginForm';
 
@@ -13,14 +15,18 @@ const localMessages = {
   title: { id: 'sources.intro.title', defaultMessage: 'Explore our Sources and Collections' },
   about: { id: 'sources.intro.about', defaultMessage: 'We add sources and create collections from media ecosystems around the world. In order to identify the right sources, we use a combination of automated search and discovery, identified lists of influential sources, and expert input from journalists and media practitioners. You can also ' },
   suggestLink: { id: 'sources.intro.suggestLink', defaultMessage: 'suggest a source.' },
-  browse: { id: 'sources.intro.browse', defaultMessage: 'Browse by Category' },
+  browseMediaCloud: { id: 'sources.into.browse.mediacloud', defaultMessage: 'Browse Media Cloud Collections' },
+  browseGlobalVoices: { id: 'sources.into.browse.mediacloud', defaultMessage: 'Browse Global Voices Collections' },
+  browseEMM: { id: 'sources.into.browse.mediacloud', defaultMessage: 'Browse European Media Monitor Collections' },
   created: { id: 'sources.intro.created', defaultMessage: "Collections I've created" },
   loginTitle: { id: 'sources.intro.login.title', defaultMessage: 'Have an Account? Login Now' },
 };
 
 const Homepage = (props) => {
+  const { goToUrl, user } = props;
+  const { formatMessage } = props.intl;
   let sideBarContent;
-  if (props.user.isLoggedIn) {
+  if (user.isLoggedIn) {
     sideBarContent = <FavoriteSourcesAndCollectionsContainer />;
   } else {
     sideBarContent = (
@@ -51,9 +57,32 @@ const Homepage = (props) => {
           {sideBarContent}
         </Col>
       </Row>
+      <Row center="lg">
+        <Col lg={4} xs={12}>
+          <AppButton
+            primary
+            label={formatMessage(localMessages.browseMediaCloud)}
+            onClick={() => { goToUrl('/collectons/media-cloud'); }}
+          />
+        </Col>
+        <Col lg={4} xs={12}>
+          <AppButton
+            primary
+            label={formatMessage(localMessages.browseGlobalVoices)}
+            onClick={() => { goToUrl('/collectons/global-voices'); }}
+          />
+        </Col>
+        <Col lg={4} xs={12}>
+          <AppButton
+            primary
+            label={formatMessage(localMessages.browseEMM)}
+            onClick={() => { goToUrl('/collectons/european-media-monitor'); }}
+          />
+        </Col>
+      </Row>
       <Row>
         <Col lg={12} xs={12}>
-          <PopularCollectionsContainer title={localMessages.browse} />
+          <PopularCollectionsContainer />
         </Col>
       </Row>
     </Grid>
@@ -67,15 +96,23 @@ Homepage.propTypes = {
   params: React.PropTypes.object.isRequired,       // params from router
   // from state
   user: React.PropTypes.object.isRequired,
+  // from dispatch
+  goToUrl: React.PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   user: state.user,
 });
 
+const mapDispatchToProps = dispatch => ({
+  goToUrl: (url) => {
+    dispatch(push(url));
+  },
+});
+
 export default
   injectIntl(
-    connect(mapStateToProps)(
+    connect(mapStateToProps, mapDispatchToProps)(
       Homepage
     )
   );
