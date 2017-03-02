17b2f40031635aa31cac666ce8f7290cb0282421
Rahul B <rahulbot@gmail.com>
2017-03-02 17:08:50 -0500

    support unselecting topic/src/collection ids in reducer

--- src/reducers/sources/collections/selected/selected.js
+++ src/reducers/sources/collections/selected/selected.js
@@ -12,7 +12,7 @@ const INITIAL_STATE = null;
 function id(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SELECT_COLLECTION:
-      return action.payload ? parseInt(action.payload.id, 10) : state;
+      return action.payload ? parseInt(action.payload, 10) : null;
     default:
       return state;
   }
