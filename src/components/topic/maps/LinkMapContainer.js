2d262a7a21ad650d7da5babe6dafe7b8ce7cd0de
cindyb <cindyloo_home at yahoo.com>
2017-03-15 17:16:19 -0400

    #258 sigma layout for topics

--- src/components/topic/maps/LinkMapContainer.js
+++ src/components/topic/maps/LinkMapContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Title from 'react-title-component';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import { Sigma, RandomizeNodePositions, RelativeSize } from 'react-sigma';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import LinkMapForm from './LinkMapForm';
 import { selectTopic, filterBySnapshot, filterByFocus, filterByTimespan } from '../../../actions/topicActions';
@@ -11,37 +10,35 @@ import { generateParamStr } from '../../../lib/apiUtil';
 const localMessages = {
   title: { id: 'topic.maps.link.title', defaultMessage: 'Link Map' },
   intro: { id: 'topic.maps.link.title', defaultMessage: 'Generate a network graph showing how the media sources linked to each other.' },
-  fetchButton: { id: 'topic.maps.button', defaultMessage: 'Download network map' },
+  downloadButton: { id: 'topic.maps.downloadbutton', defaultMessage: 'Download a network map' },
+  viewButton: { id: 'topic.maps.viewbutton', defaultMessage: 'View a network map' },
 };
 
 class LinkMapContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewMap: false,
+    };
+  }
   componentWillReceiveProps(nextProps) {
     const { fetchData, filters } = this.props;
     if (nextProps.filters.timespanId !== filters.timespanId) {
       fetchData(nextProps);
     }
   }
-
+  toggleViewMap = () => {
+    this.setState({
+      viewMap: !this.state.viewMap,
+    });
+  }
   render() {
-    const { handleFetchMapData } = this.props;
+    const { handleFetchMapData, filters, topicId } = this.props;
     const { formatMessage } = this.props.intl;
     const titleHandler = parentTitle => `${formatMessage(localMessages.title)} | ${parentTitle}`;
     const initialValues = { color_field: 'media_type', num_media: 500, include_weights: false };
-    const nodes = { nodes: [{ id: 'n3', label: 'Alice' }, { id: 'n2', label: 'Rabbit' }], edges: [{ id: 'e1', source: 'n3', target: 'n2', label: 'SEES' }] };
 
-    const sigmaElement = (
-      <Sigma
-        graph={{ nodes: [{ id: 'id0' }, { id: 'id1' }], edges: [{ id: 'e0', source: 'id0', target: 'id1' }] }}
-        settings={{ drawEdges: true }}
-      >
-        <RelativeSize initialSize={15} />
-        <RandomizeNodePositions />
-      </Sigma>
-    );
-    if (sigmaElement && sigmaElement.graph) {
-      sigmaElement.graph.addNodes(nodes);
-    }
     return (
       <Grid>
         <Title render={titleHandler} />
@@ -51,18 +48,16 @@ class LinkMapContainer extends React.Component {
             <p><FormattedMessage {...localMessages.intro} /></p>
           </Col>
         </Row>
-        <Row>
-          <Col lg={6} md={6} sm={12}>
-            <LinkMapForm
-              initialValues={initialValues}
-              onFetch={handleFetchMapData}
-              buttonLabel={formatMessage(localMessages.fetchButton)}
-            />
-          </Col>
-          <Col lg={6} md={6} sm={12}>
-            {sigmaElement}
-          </Col>
-        </Row>
+        <LinkMapForm
+          initialValues={initialValues}
+          filters={filters}
+          topicId={topicId}
+          showMap={this.state.viewMap}
+          onGetMapData={handleFetchMapData}
+          onViewMapData={this.toggleViewMap}
+          viewLabel={formatMessage(localMessages.viewButton)}
+          downloadLabel={formatMessage(localMessages.downloadButton)}
+        />
       </Grid>
     );
   }
@@ -91,8 +86,8 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   handleFetchMapData: (values) => {
     // try to save it
     const params = generateParamStr({ ...ownProps.location.query, ...values });
-
     const url = `/api/topics/${ownProps.params.topicId}/map-files/fetchCustomMap?${params}`;
+
     window.location = url;
   },
   fetchData: () => {
