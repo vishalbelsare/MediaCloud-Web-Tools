a6f76975480b6cf86aa07d2cc4ae45157b5b0676
Rahul B <rahulbot@gmail.com>
2017-03-28 14:12:46 -0400

    add user routes to tools app, make it work for public users (#324)

--- src/routes/toolsRoutes.js
+++ src/routes/toolsRoutes.js
@@ -3,11 +3,17 @@ import Route from 'react-router/lib/Route';
 import IndexRedirect from 'react-router/lib/IndexRedirect';
 import ToolsApp from '../components/tools/ToolsApp';
 import ToolsHomeContainer from '../components/tools/ToolsHomeContainer';
+import userRoutes from './userRoutes';
 
 const toolsRoutes = (
   <Route path="/" component={ToolsApp}>
+
     <IndexRedirect to="/home" />
+
     <Route path="home" component={ToolsHomeContainer} />
+
+    {userRoutes}
+
   </Route>
 );
 
