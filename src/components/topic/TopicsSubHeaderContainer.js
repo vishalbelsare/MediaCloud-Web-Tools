1264e99280d0d51d410e636a3729fa78000f28a6
Rahul B <rahulbot@gmail.com>
2017-04-14 14:06:42 -0400

    renamed class correctly

--- src/components/topic/TopicsSubHeaderContainer.js
+++ src/components/topic/TopicsSubHeaderContainer.js
@@ -11,7 +11,7 @@ const localMessages = {
   topicUnfavorited: { id: 'source.unfavorited', defaultMessage: 'Unstar this topic' },
 };
 
-const SourceMgrSubHeaderContainer = (props) => {
+const TopicMgrSubHeaderContainer = (props) => {
   const { topicId, topicInfo, handleSetFavorited } = props;
   const { formatMessage } = props.intl;
   let title = '';
@@ -41,7 +41,7 @@ const SourceMgrSubHeaderContainer = (props) => {
   );
 };
 
-SourceMgrSubHeaderContainer.propTypes = {
+TopicMgrSubHeaderContainer.propTypes = {
   // from parent
   // from context
   intl: React.PropTypes.object.isRequired,
@@ -70,6 +70,6 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 export default
   injectIntl(
     connect(mapStateToProps, mapDispatchToProps)(
-      SourceMgrSubHeaderContainer
+      TopicMgrSubHeaderContainer
     )
   );
