93b88888c82e72686c5e1159230db9221abaab9b
Rahul B <rahulbot@gmail.com>
2017-04-30 11:10:37 -0400

    remove topic list caching (#407)

--- server/views/topics/__init__.py
+++ server/views/topics/__init__.py
@@ -11,14 +11,6 @@ logger = logging.getLogger(__name__)
 SORT_SOCIAL = 'social'
 SORT_INLINK = 'inlink'
 
-try:
-    topics_data = json.load(open(os.path.join(base_dir, 'server', 'static', 'data', 'all_topics.json')))
-    CACHED_TOPICS = topics_data['topics']
-    IS_CACHED = True
-except IOError as e:
-    logger.warning("Run update_topic_list.py to generate the cached topic list!")
-    CACHED_TOPICS = []
-    IS_CACHED = False
 
 def validated_sort(desired_sort, default_sort=SORT_SOCIAL):
     valid_sorts = [SORT_SOCIAL, SORT_INLINK]
@@ -26,14 +18,15 @@ def validated_sort(desired_sort, default_sort=SORT_SOCIAL):
         return default_sort
     return desired_sort
 
+
 def topic_is_public(topics_id):
     topic = mc.topic(topics_id)
     is_public = topic['is_public']
-    # return bool(is_public)
-    return True
+    return int(is_public)== 1
+
 
 def access_public_topic(topics_id):
     # check whether logged in here since it is a requirement for public access
-    if ((not is_user_logged_in()) and (topic_is_public(topics_id))):
+    if (not is_user_logged_in()) and (topic_is_public(topics_id)):
         return True
     return False
