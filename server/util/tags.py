ee9ed0d1b8ea127086bf63161a71f1a340aa225c
cindyb <cindyloo_home at yahoo.com>
2017-05-19 14:53:54 -0400

    #430 constants for tags/themes

--- server/util/tags.py
+++ server/util/tags.py
@@ -4,6 +4,10 @@ NYT_LABELER_1_0_0_TAG_ID = 9360669  # the tag that indicates a story was tagged 
 NYT_LABELS_TAG_SET_ID = 1963        # the tag set all the descriptor tags are in
 NYT_LABELS_SAMPLE_SIZE = 10000      # the sample size to use for looking at NYT descriptor tags
 
+#constants for versioning of geo-tagged stories
+TAG_SET_GEOCODER_VERSION = 1937;
+TAG_SET_NYT_LABELS_VERSION = 1964;
+
 # constants related to the CLIFF-based geotagging (ie. tags on stories indicating places they are about)
 CLIFF_CLAVIN_2_3_0_TAG_ID = 9353691  # the tag that indicates a story was tagged by the CLIFF version 2.3.0
 GEO_TAG_SET = 1011                   # the tag set all the geo tags are in
