9ed0f803fc89405dcaead50781f00510852dddbd
cindyb <cindyloo_home at yahoo.com>
2017-05-24 16:57:09 -0400

    Refactoring name change- a couple of conflicts that should be resolved here

--- server/views/sources/metadata.py
+++ server/views/sources/metadata.py
@@ -18,7 +18,7 @@ def api_metadata_values(tag_sets_id):
     Source metadata is encoded in varous tag sets - this returns the set and the list of
     available tags you can use
     '''
-    user_mc = user_mediacloud_client()
+    user_mc = user_admin_mediacloud_client()
     tag_set = user_mc.tagSet(tag_sets_id)
     tag_set['tags'] = cached_tags_in_tag_set(tag_sets_id)
     return jsonify(tag_set)
