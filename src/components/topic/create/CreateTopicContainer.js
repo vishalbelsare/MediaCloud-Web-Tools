66939633a78b1acbaf96731ba5f0ebd6bdb1977c
cindyb <cindyloo_home at yahoo.com>
2017-05-24 11:15:06 -0400

    #451

--- src/components/topic/create/CreateTopicContainer.js
+++ src/components/topic/create/CreateTopicContainer.js
@@ -5,15 +5,16 @@ import composeAsyncContainer from '../../common/AsyncContainer';
 import { fetchUserQueuedAndRunningTopics } from '../../../actions/topicActions';
 import { WarningNotice } from '../../common/Notice';
 import TopicBuilderWizard from './TopicBuilderWizard';
+import { getUserRoles, hasPermissions, PERMISSION_TOPIC_ADMIN } from '../../../lib/auth';
 
 const localMessages = {
   cannotCreateTopic: { id: 'topic.create.cannotCreateTopic', defaultMessage: 'You cannot create a new topic right now because you are currently running another topic: {name} #{id}' },
 };
 
 const CreateTopicContainer = (props) => {
-  const { canCreateTopic, runningTopics } = props;
+  const { canCreateTopic, runningTopics, user } = props;
 
-  if (!canCreateTopic) {
+  if (!hasPermissions(getUserRoles(user), PERMISSION_TOPIC_ADMIN) && !canCreateTopic) {
     return (
       <WarningNotice><FormattedHTMLMessage {...localMessages.cannotCreateTopic} values={{ name: runningTopics[0].name, id: runningTopics[0].topics_id }} /></WarningNotice>
     );
@@ -31,12 +32,14 @@ CreateTopicContainer.propTypes = {
   intl: React.PropTypes.object.isRequired,
   canCreateTopic: React.PropTypes.bool,
   runningTopics: React.PropTypes.array,
+  user: React.PropTypes.object,
 };
 
 const mapStateToProps = state => ({
   fetchStatus: state.topics.create.userRunningTopicStatus.fetchStatus,
   canCreateTopic: state.topics.create.userRunningTopicStatus.allowed,
   runningTopics: state.topics.create.userRunningTopicStatus.runningTopics,
+  user: state.user,
 });
 
 const mapDispatchToProps = dispatch => ({
