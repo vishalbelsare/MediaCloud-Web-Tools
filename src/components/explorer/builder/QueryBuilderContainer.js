f491a3bff469a11144c87bf5303e1fd52ddf3675
cindyb <cindyloo_home at yahoo.com>
2017-06-05 18:39:22 -0400

    #465 decide to do id vs query string in URL

--- src/components/explorer/builder/QueryBuilderContainer.js
+++ src/components/explorer/builder/QueryBuilderContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import { selectQuery } from '../../../actions/explorerActions';
+import { Grid } from 'react-flexbox-grid/lib';
+import { selectQuery, setQueryList } from '../../../actions/explorerActions';
 import QueryForm from './QueryForm';
 import QueryPicker from './QueryPicker';
-import { notEmptyString } from '../../../lib/formValidators';
+// import { notEmptyString } from '../../../lib/formValidators';
 import { getUserRoles, hasPermissions, PERMISSION_LOGGED_IN } from '../../../lib/auth';
 
 const localMessages = {
@@ -14,21 +15,18 @@ const localMessages = {
 
 class QueryBuilderContainer extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      initialQuery: '',
-    };
-  }
-
   componentWillReceiveProps(nextProps) {
     const { selected, setSelectedQuery } = this.props;
     // TODO better comparison
-    if (selected === null || (notEmptyString(nextProps.urlQueryString.query) && nextProps.urlQueryString.query !== JSON.stringify(selected))) {
+    // if a query is clicked or if the url is edited...
+    // THis is definitely not working right..
+    if (selected === null || nextProps.selected === null) {
       // make sure this is a valid query string
       // if not logged in, ignore/strip out anything but keywords?
       // const var1 = '{"keyword": "public"}';
       // TODO formatting of query or param
+
+      // but what if clicked? this is changing the selection, not the query string
       const qObject = JSON.parse(nextProps.urlQueryString.query);
       setSelectedQuery(qObject);
     }
@@ -38,24 +36,25 @@ class QueryBuilderContainer extends React.Component {
   }
 
   render() {
-    const { selected, queries, handleSearch, user } = this.props;
+    const { selected, queries, handleSearch, setSelectedQuery, user } = this.props;
     const { formatMessage } = this.props.intl;
-    const isLoggedInUser = hasPermissions(getUserRoles(user), PERMISSION_LOGGED_IN);
+    const isNotLoggedInUser = !(hasPermissions(getUserRoles(user), PERMISSION_LOGGED_IN));
     let content = <div>Error</div>;
 
+    // TODO problem with initialValues not updating wr to selected value...
     if (queries && queries.length > 0 && selected) {
       content = (
         <div>
-          <QueryPicker selected={selected} queries={queries} isEditable={isLoggedInUser} />
-          <QueryForm initialValues={selected} buttonLabel={formatMessage(localMessages.querySearch)} onSave={handleSearch} isEditable={isLoggedInUser} />
+          <QueryPicker selected={selected} queries={queries} isEditable={isNotLoggedInUser} onClick={setSelectedQuery} />
+          <QueryForm initialValues={selected} buttonLabel={formatMessage(localMessages.querySearch)} onSave={handleSearch} isEditable={isNotLoggedInUser} />
         </div>
       );
     }
     // TODO, decide whether to show QueryForm if in Demo mode
     return (
-      <div>
+      <Grid>
         { content }
-      </div>
+      </Grid>
     );
   }
 
@@ -82,19 +81,27 @@ const mapStateToProps = (state, ownProps) => ({
   user: state.user,
 });
 
+// push any updates (including selected) into queries in state, will trigger async load in sub sections
 const mapDispatchToProps = dispatch => ({
   handleSearch: (queries) => {
     const infoToQuery = queries;
     // TODO if in Demo mode, constrain by two-week period
     // assimilate field array level query info
-    dispatch(selectQuery(infoToQuery));
-    // will trigger async load in sub sections
+    // will push new/custom query into list as well
+    dispatch(setQueryList(infoToQuery));
   },
-  setSelectedQuery: (queryObj) => {
-    // push url data from params into store
-    // this should setup initialValues
-    dispatch(selectQuery(queryObj)); // where to push into queries object?
-    // dispatch(fetchSavedQueryList());
+  // this will push selected into state
+  // either an id (demo) or a query url (loggedin)
+  setSelectedQuery: (queryType) => {
+    // const isLoggedInUser = hasPermissions(getUserRoles(state.user), PERMISSION_LOGGED_IN);
+
+    // if (isLoggedInUser) {
+    //  dispatch(selectQuery(queryType)); // query string - we will select the custom tab for them unless there is an id in url
+      // dispatch(fetchSavedQueryList());
+    // } else {
+    dispatch(selectQuery(queryType)); // query id
+      // dispatch(fetchExampleQueryList());
+    // }
   },
 });
 
