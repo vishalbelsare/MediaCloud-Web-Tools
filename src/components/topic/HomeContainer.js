3d718c3fb060bc7f712134a1470bbc6cdf95e8a8
Rahul B <rahulbot@gmail.com>
2017-06-08 15:46:58 -0400

    specifically redirect from /home to public home if user isn't logged in

--- src/components/topic/HomeContainer.js
+++ src/components/topic/HomeContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from 'react-title-component';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
 import TopicSearchContainer from './search/TopicSearchContainer';
@@ -9,31 +11,61 @@ const localMessages = {
   homeTitle: { id: 'home.title', defaultMessage: 'Home' },
 };
 
-const HomeContainer = (props) => {
-  const { formatMessage } = props.intl;
-  const title = formatMessage(localMessages.homeTitle);
-  const titleHandler = parentTitle => `${title} | ${parentTitle}`;
-  return (
-    <div>
-      <Title render={titleHandler} />
-      <div className="controlbar">
-        <div className="main">
-          <Grid>
-            <Row>
-              <Col lg={8} />
-              <Col lg={4}>
-                <TopicSearchContainer />
-              </Col>
-            </Row>
-          </Grid>
+class HomeContainer extends React.Component {
+
+  componentWillReceiveProps(nextProps) {
+    // make sure logout on home goes to public home
+    const { redirectToPublicHome } = this.props;
+    if (nextProps.isLoggedIn === false) {
+      redirectToPublicHome();
+    }
+  }
+
+  render() {
+    const { formatMessage } = this.props.intl;
+    const title = formatMessage(localMessages.homeTitle);
+    const titleHandler = parentTitle => `${title} | ${parentTitle}`;
+    return (
+      <div>
+        <Title render={titleHandler} />
+        <div className="controlbar">
+          <div className="main">
+            <Grid>
+              <Row>
+                <Col lg={8} />
+                <Col lg={4}>
+                  <TopicSearchContainer />
+                </Col>
+              </Row>
+            </Grid>
+          </div>
         </div>
+        <Grid>
+          <TopicListContainer />
+        </Grid>
       </div>
-      <Grid>
-        <TopicListContainer />
-      </Grid>
-    </div>
-  );
+    );
+  }
+
+}
+
+HomeContainer.propTypes = {
+  // from state
+  isLoggedIn: React.PropTypes.bool.isRequired,
+  // from dispatch
+  redirectToPublicHome: React.PropTypes.func.isRequired,
 };
+
+const mapStateToProps = state => ({
+  userTopicPermission: state.topics.selected.info.user_permission,
+  isLoggedIn: state.user.isLoggedIn,
+});
+
+const mapDispatchToProps = dispatch => ({
+  redirectToPublicHome: () => {
+    dispatch(push('/topics/public/home'));
+  },
+});
 
 HomeContainer.propTypes = {
   // from context
@@ -42,5 +74,7 @@ HomeContainer.propTypes = {
 
 export default
   injectIntl(
-    HomeContainer
+    connect(mapStateToProps, mapDispatchToProps)(
+      HomeContainer
+    )
   );
