92a7a8605bb3bce765d7ee8598f769ff57721bd8
cindyb <cindyloo_home at yahoo.com>
2017-06-08 13:50:17 -0400

    #459 add metadata to topics download. Refactor common code
    Also add facebook_share_id

--- server/views/topics/__init__.py
+++ server/views/topics/__init__.py
@@ -12,6 +12,11 @@ logger = logging.getLogger(__name__)
 SORT_SOCIAL = 'social'
 SORT_INLINK = 'inlink'
 
+TOPICS_TEMPLATE_PROPS = ['media_id', 'name', 'url', 'story_count',
+                 'media_inlink_count', 'sum_media_inlink_count', 'inlink_count',
+                 'outlink_count', 'bitly_click_count', 'facebook_share_count',
+                 'pub_country', 'pub_state', 'primary_language']
+
 
 def validated_sort(desired_sort, default_sort=SORT_SOCIAL):
     valid_sorts = [SORT_SOCIAL, SORT_INLINK]
