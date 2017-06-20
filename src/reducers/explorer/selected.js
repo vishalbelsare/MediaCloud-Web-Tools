c16141c83243a69613f3c38254e0a92014e90b58
cindyb <cindyloo_home at yahoo.com>
2017-06-20 15:30:47 -0400

    #465 remove superfluous field

--- src/reducers/explorer/selected.js
+++ src/reducers/explorer/selected.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = null;
 function selected(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SELECT_QUERY:
-      return action.payload ? { ...action.payload, q: action.payload.q } : null;
+      return action.payload ? { ...action.payload } : null;
     case UPDATE_QUERY:
       return action.payload ? { ...state, ...action.payload } : null;
     default:
