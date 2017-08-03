2e4a40da921670fb4bfe6ea44b54688fa2dada8b
Rahul B <rahulbot@gmail.com>
2017-08-03 15:50:09 -0400

    cleanup JS in test helper

--- test/helpers/visit.js
+++ test/helpers/visit.js
@@ -1,49 +1,49 @@
-import nightmare from 'nightmare'
-import url from 'url'
+import nightmare from 'nightmare';
+import url from 'url';
 
 const BASE_URL = url.format({
-  protocol : process.env.PROTOCOL || 'http',
-  hostname : process.env.HOST || 'www.localhost',
-  port     : process.env.PORT || 5000
-})
+  protocol: process.env.PROTOCOL || 'http',
+  hostname: process.env.HOST || 'www.localhost',
+  port: process.env.PORT || 5000,
+});
 
-const DEBUG = process.env.DEBUG || false
+const DEBUG = process.env.DEBUG || false;
 
-export default function (path='', query={}) {
-  const location = url.resolve(BASE_URL, path)
+export default function (path = '' /*, query = {}*/) {
+  const location = url.resolve(BASE_URL, path);
 
   /* add custom actions here */
   nightmare.action('touchTap', function(selector, done) {
-  	this.evaluate_now((selector) => {
-  		var t = new Touch({
-		    identifier: Date.now(),
-		    target: document.querySelector(selector),
-		    clientX: 100,
-		    clientY: 100,
-		    radiusX: 2.5,
-		    radiusY: 2.5,
-		    rotationAngle: 10,
-		    force: 0.5,
-		  });
+    this.evaluate_now((selector) => {
+      const t = new Touch({
+        identifier: Date.now(),
+        target: document.querySelector(selector),
+        clientX: 100,
+        clientY: 100,
+        radiusX: 2.5,
+        radiusY: 2.5,
+        rotationAngle: 10,
+        force: 0.5,
+      });
 
-		  var touchEvent = new TouchEvent('touchend', {
-		    cancelable: true,
-		    bubbles: true,
-		    touches: [t],
-		    targetTouches: [],
-		    changedTouches: [t],
-		    shiftKey: true,
-		  });
+      const touchEvent = new TouchEvent('touchend', {
+        cancelable: true,
+        bubbles: true,
+        touches: [t],
+        targetTouches: [],
+        changedTouches: [t],
+        shiftKey: true,
+      });
 
-		  document.querySelector(selector).dispatchEvent(touchEvent);
-  	}, done, selector);
+      document.querySelector(selector).dispatchEvent(touchEvent);
+    }, done, selector);
   });
 
   /* Create new nightmare instance */
   const page = nightmare({
     show: true,
-    pollInterval: 50
-  })
+    pollInterval: 50,
+  });
 
-  return page.goto(location)
+  return page.goto(location);
 }
