6b17dde6b19e6f596ade518f14c1cb31cd1f7c56
Rahul B <rahulbot@gmail.com>
2017-08-24 10:25:45 -0400

    fix alt tag on tool homepage images

--- src/components/tools/ToolDescription.js
+++ src/components/tools/ToolDescription.js
@@ -4,11 +4,12 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 
 const ToolDescription = (props) => {
   const { name, description, screenshotUrl, url, className } = props;
+  const { formatMessage } = props.intl;
   return (
     <div className={`tool-description ${className}`} onTouchTap={() => { window.location = url; }}>
       <h2><FormattedMessage {...name} /></h2>
       <p><FormattedMessage {...description} /></p>
-      <img src={screenshotUrl} alt={<FormattedMessage {...name} />} width="100%" />
+      <img src={screenshotUrl} alt={formatMessage(name)} width="100%" />
     </div>
   );
 };
@@ -17,8 +18,8 @@ ToolDescription.propTypes = {
   // from composition chain
   intl: PropTypes.object.isRequired,
   // from parent
-  name: PropTypes.object.isRequired,
-  description: PropTypes.object.isRequired,
+  name: PropTypes.object.isRequired,  // a msg to intl
+  description: PropTypes.object.isRequired,  // a msg to intl
   screenshotUrl: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
