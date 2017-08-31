767b29917ca9e61fb6e5b771818701fb4ec42ee3
Rahul B <rahulbot@gmail.com>
2017-08-30 22:19:36 -0400

    fetch retweet story counts in parallel

--- server/views/topics/foci/retweetpartisanship.py
+++ server/views/topics/foci/retweetpartisanship.py
@@ -1,6 +1,7 @@
 import logging
 from flask import jsonify, request
 import flask_login
+from multiprocessing import Pool
 
 from server import app
 from server.cache import cache
@@ -13,24 +14,28 @@ from server.views.topics.foci import FOCAL_TECHNIQUE_BOOLEAN_QUERY
 logger = logging.getLogger(__name__)
 
 
+def _retweet_story_count_worker(job):
+    tagged_story_count = topic_story_count(user_mediacloud_key(), job['topics_id'], q=job['tag']['media_query'])['count']
+    return {
+        'label': job['tag']['label'],
+        'tags_id': job['tag']['tags_id'],
+        'count': tagged_story_count,
+        'pct': float(tagged_story_count) / float(job['total_stories'])
+    }
+
 @app.route('/api/topics/<topics_id>/focal-sets/retweet-partisanship/preview/story-counts', methods=['GET'])
 @flask_login.login_required
 @api_error_handler
 def retweet_partisanship_story_counts(topics_id):
     # TODO: add in overall timespan id here so it works in different snapshots
-    tag_story_counts = []
     partisanship_tags = cached_media_tags(TAG_SETS_ID_RETWEET_PARTISANSHIP_2016)
     # grab the total stories
     total_stories = topic_story_count(user_mediacloud_key(), topics_id)['count']
-    # make a count for each tag based on media_od
-    for tag in partisanship_tags:
-        tagged_story_count = topic_story_count(user_mediacloud_key(), topics_id, q=tag['media_query'])['count']
-        tag_story_counts.append({
-            'label': tag['label'],
-            'tags_id': tag['tags_id'],
-            'count': tagged_story_count,
-            'pct': float(tagged_story_count)/float(total_stories)
-        })
+    # make a count for each tag based on media_id (parallel for speed and to avoid server timeouts)
+    jobs = [{'topics_id': topics_id, 'tag': tag, 'total_stories': total_stories} for tag in partisanship_tags]
+    pool = Pool(processes=len(partisanship_tags))
+    tag_story_counts = pool.map(_retweet_story_count_worker, jobs)  # blocks until they are all done
+    pool.terminate()  # extra safe garbage collection
     # order them in the way a person would expect ( left to center to right)
     ordered_tag_story_counts = list()
     ordered_tag_story_counts.append([t for t in tag_story_counts if t['tags_id'] == 9360520][0])
