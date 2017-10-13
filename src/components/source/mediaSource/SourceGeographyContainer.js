d43513f97e0c6eee475f915fc201b95ebf62e962
Rahul B <rahulbot@gmail.com>
2017-10-13 13:05:47 -0400

    switch map help text to focus, not mentioned (#672)

--- src/components/source/mediaSource/SourceGeographyContainer.js
+++ src/components/source/mediaSource/SourceGeographyContainer.js
@@ -15,7 +15,7 @@ const localMessages = {
   title: { id: 'source.summary.map.title', defaultMessage: 'Geographic Attention' },
   helpTitle: { id: 'source.summary.map.help.title', defaultMessage: 'Geographic Attention' },
   intro: { id: 'source.summary.map.intro',
-    defaultMessage: '<p>Here is a heatmap of countries mentioned by this source (based on a sample of sentences). Darker countried are mentioned more. Click a country to load a Dashboard search showing you how the this source covers it.</p>' },
+    defaultMessage: '<p>Here is a heatmap of countries stories from this source are about (based on a sample of sentences). Darker countried are mentioned more. Click a country to load a Dashboard search showing you how the this source covers it.</p>' },
 };
 
 class SourceGeographyContainer extends React.Component {
