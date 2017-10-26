60648b1c79edc7b08d977ab7397e5a2b78afcc63
cindyb <cindyloo_home at yahoo.com>
2017-10-26 13:20:48 -0400

    #693 provide autoLink parameter for default source manager linking

--- src/components/topic/summary/TopicInfo.js
+++ src/components/topic/summary/TopicInfo.js
@@ -84,7 +84,7 @@ const TopicInfo = (props) => {
       <p>
         <b><FormattedHTMLMessage {...messages.topicSourceCollectionsProp} /></b>
       </p>
-      {sourcesAndCollections.map(object => <SourceOrCollectionChip key={object.tags_id || object.media_id} object={object} />)}
+      {sourcesAndCollections.map(object => <SourceOrCollectionChip key={object.tags_id || object.media_id} object={object} autoLink />)}
       <p>
         <b><FormattedHTMLMessage {...messages.topicValidationProp} /></b>
         <code>{topic.pattern}</code>
