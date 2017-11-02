28938f03b3954745fb38f18c72b484b919f9c3ff
Rahul B <rahulbot@gmail.com>
2017-11-02 16:41:30 -0400

    Revert "Fix toolbar back to dashboard for builds"
    This reverts commit 90ad6b8bf2cc730683c1b0b34935660ba2d1f38f.

--- src/components/common/header/AppToolbar.js
+++ src/components/common/header/AppToolbar.js
@@ -7,7 +7,6 @@ import UserMenuContainer from './UserMenuContainer';
 
 export const TOPICS_URL = 'https://topics.mediacloud.org/';
 export const EXPLORER_URL = 'https://explorer.mediacloud.org/';
-export const DASHBOARD_URL = 'https://dashboard.mediacloud.org/';
 export const SOURCES_URL = 'https://sources.mediacloud.org/';
 export const BLOG_URL = 'http://mediacloud.org/';
 export const TOOLS_URL = 'https://tools.mediacloud.org/';
@@ -30,24 +29,12 @@ const AppToolbar = (props) => {
                   <FormattedMessage {...messages.toolsAppName} />
                 </a>
               </li>
-              {
-              /*
               <li className="explorer">
                 <a
                   href={EXPLORER_URL}
                   title={formatMessage(messages.explorerToolDescription)}
                 >
                   <FormattedMessage {...messages.explorerToolName} />
-                </a>
-              </li>
-              */
-              }
-              <li className="dashboard">
-                <a
-                  href={DASHBOARD_URL}
-                  title={formatMessage(messages.dashboardToolDescription)}
-                >
-                  <FormattedMessage {...messages.dashboardToolName} />
                 </a>
               </li>
               <li className="topics">
