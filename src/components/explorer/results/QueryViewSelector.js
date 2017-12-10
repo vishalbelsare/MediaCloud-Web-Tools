91e088c2d5b3cb607628b001398e1ad94c775cf1
Rahul B <rahulbot@gmail.com>
2017-12-10 12:07:01 -0500

    style work on explorer tabs (#736)

--- src/components/explorer/results/QueryViewSelector.js
+++ src/components/explorer/results/QueryViewSelector.js
@@ -1,5 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { injectIntl } from 'react-intl';
+
+
+const localMessages = {
+  attention: { id: 'explorer.results.attention.title', defaultMessage: 'Attention' },
+  language: { id: 'explorer.results.language.title', defaultMessage: 'Language' },
+  people: { id: 'explorer.results.people.title', defaultMessage: 'People' },
+  reach: { id: 'explorer.results.reach.title', defaultMessage: 'Reach' },
+};
 
 class QueryViewSelector extends React.Component {
   state = {
@@ -8,11 +17,12 @@ class QueryViewSelector extends React.Component {
 
   render() {
     const { onViewSelected } = this.props;
+    const { formatMessage } = this.props.intl;
     const menuOptions = [
-      { label: 'ATTENTION', index: 0 },
-      { label: 'LANGUAGE', index: 1 },
-      { label: 'PEOPLE', index: 2 },
-      { label: 'REACH', index: 3 },
+      { label: formatMessage(localMessages.attention), index: 0 },
+      { label: formatMessage(localMessages.language), index: 1 },
+      { label: formatMessage(localMessages.people), index: 2 },
+      { label: formatMessage(localMessages.reach), index: 3 },
     ];
     return (
       <ul className="query-view-selector">
@@ -40,6 +50,11 @@ QueryViewSelector.propTypes = {
   // from parent
   options: PropTypes.array,
   onViewSelected: PropTypes.func,
+  // from compositional chain
+  intl: PropTypes.object.isRequired,
 };
 
-export default QueryViewSelector;
+export default
+  injectIntl(
+    QueryViewSelector
+  );
