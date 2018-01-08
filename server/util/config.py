dd4c028ae0ecddf4e471ee707d5185a8ec1dca97
Rahul B <rahulbot@gmail.com>
2018-01-08 12:49:13 -0500

    support for env-var based config (#769)

--- server/util/config.py
+++ server/util/config.py
@@ -10,19 +10,22 @@ logger = logging.getLogger(__name__)
 def _variables_from_file(filepath):
     vars = {}
     logger.info(u"Loading configuration from {}".format(filepath))
-    f = codecs.open(filepath, 'r', 'utf-8')
-    for line in f:
-        if len(line.strip()) is 0:
-            continue
-        if line.strip()[0] is COMMENT_CHAR:
-            continue
-        parts = line.split(u"=")
-        key = parts[0].strip().upper()
-        value = parts[1].strip()
-        logger.debug(u"  {}={}".format(key, value))
-        if key in vars:
-            raise ConfigException(u"Config variable '{}' is declared twice in {}".format(key, filepath))
-        vars[key] = value
+    try:
+        f = codecs.open(filepath, 'r', 'utf-8')
+        for line in f:
+            if len(line.strip()) is 0:
+                continue
+            if line.strip()[0] is COMMENT_CHAR:
+                continue
+            parts = line.split(u"=")
+            key = parts[0].strip().upper()
+            value = parts[1].strip()
+            logger.debug(u"  {}={}".format(key, value))
+            if key in vars:
+                raise ConfigException(u"Config variable '{}' is declared twice in {}".format(key, filepath))
+            vars[key] = value
+    except IOError:
+        logger.info(u"No local app.config file found; relying on environment variables for configuration")
     return vars
 
 
@@ -37,7 +40,7 @@ class EnvOrFileBasedConfig(object):
 
     def __init__(self, absolute_filepath):
         self.file_path = absolute_filepath
-        self.variables = _variables_from_file(self.file_path) # a dict from VARIABLE_NAME to value
+        self.variables = _variables_from_file(self.file_path)  # a dict from VARIABLE_NAME to value
 
     def get(self, key):
         variable_name = key.upper()
@@ -47,11 +50,12 @@ class EnvOrFileBasedConfig(object):
             try:
                 return self.variables[variable_name]
             except KeyError:
-                raise ConfigException(u"Config variable '{}' not declared in env-var nor in {}".format(
-                    variable_name, self.file_path))
+                logger.error(error_details)
+                raise ConfigException(error_details)
 
 
 def get_default_config():
     base_dir = os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
     path_to_config_file = os.path.join(base_dir, 'config', 'app.config')
-    return EnvOrFileBasedConfig(path_to_config_file)
+    config = EnvOrFileBasedConfig(path_to_config_file)
+    return config
