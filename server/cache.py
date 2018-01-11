6eb5bbc6c9d583a94131953a67ef739fe5f61091
cindyb <cindyloo_home at yahoo.com>
2018-01-11 09:58:02 -0500

    #742 in keeping with python style

--- server/cache.py
+++ server/cache.py
@@ -1,5 +1,5 @@
-import hermes.backend.memcached
-cache = hermes.Hermes(hermes.backend.memcached.Backend, ttl=86400*3)  # three days
+#import hermes.backend.memcached
+#cache = hermes.Hermes(hermes.backend.memcached.Backend, ttl=86400*3)  # three days
 
-#import hermes.backend.redis
-#cache = hermes.Hermes(hermes.backend.redis.Backend, host='localhost', db=1, ttl=86400*3)  # three days
+import hermes.backend.redis
+cache = hermes.Hermes(hermes.backend.redis.Backend, host='localhost', db=1, ttl=86400*3)  # three days
