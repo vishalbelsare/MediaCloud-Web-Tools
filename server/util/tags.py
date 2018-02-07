a7f587076dc64b5d01c24bd03bd6389d21c5e22d
cindyb <cindyloo_home at yahoo.com>
2018-02-07 11:34:00 -0500

    #826

--- server/util/tags.py
+++ server/util/tags.py
@@ -23,16 +23,14 @@ CLIFF_CLAVIN_2_3_0_TAG_ID = 9353691  # the tag that indicates a story was tagged
 CLIFF_CLAVIN_2_4_1_TAG_ID = 9696677  # the tag that indicates a story was tagged by the CLIFF version 2.4.1
 GEO_TAG_SET = 1011  # the tag set all the geo tags are in
 GEO_SAMPLE_SIZE = 10000  # the sample size to use for looking at geo tags
-CLIFF_ORGS =  2388
+CLIFF_ORGS = 2388
 CLIFF_PEOPLE = 2389
 
 # Source collection tags sets
 TAG_SETS_ID_COLLECTIONS = 5  # holds all the Media Cloud collections
-TAG_SET_ID_GV = 556  # holds country collections made from scraping GlobalVoices outlinks
-TAG_SET_ID_EMM = 597  # holds country collections made from scraping European Media Monitor source list
 TAG_SET_ID_PARTISAN_RETWEETS = 1959  # holds the partisan retweet quintiles
 TAG_SET_ABYZ_GEO_COLLECTIONS = 15765102  # for geographic collections we are importing from ABYZ
-VALID_COLLECTION_TAG_SETS_IDS = [TAG_SET_ID_EMM, TAG_SET_ID_GV, TAG_SETS_ID_COLLECTIONS, TAG_SET_ID_PARTISAN_RETWEETS,
+VALID_COLLECTION_TAG_SETS_IDS = [TAG_SETS_ID_COLLECTIONS, TAG_SET_ID_PARTISAN_RETWEETS,
                                  TAG_SET_ABYZ_GEO_COLLECTIONS]
 
 # Source metadata tag sets
