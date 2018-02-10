c09a1ae543699cc4035925d1b1f9c4c9fe551da9
Rahul B <rahulbot@gmail.com>
2018-02-09 21:37:39 -0500

    Cleanup on new topic controller and header

--- src/components/topic/TopicsApp.js
+++ src/components/topic/TopicsApp.js
@@ -3,13 +3,11 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import Title from 'react-title-component';
 import AppContainer from '../AppContainer';
-import TopicsDrawer from './TopicsDrawer';
 import messages from '../../resources/messages';
 
 const TopicsApp = (props) => {
   const { formatMessage } = props.intl;
   const titleHandler = parentTitle => `${formatMessage(messages.topicsToolName)} | ${parentTitle}`;
-  const drawer = <TopicsDrawer />;
   return (
     <div>
       <Title render={titleHandler} />
@@ -17,7 +15,6 @@ const TopicsApp = (props) => {
         name="topics"
         title={formatMessage(messages.topicsToolName)}
         description={formatMessage(messages.topicsToolDescription)}
-        drawer={drawer}
       >
         {props.children}
       </AppContainer>
