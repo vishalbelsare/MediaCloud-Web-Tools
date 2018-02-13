bbb9e3f988084f7850b08b2ca4315bc2d2fd6f90
Rahul B <rahulbot@gmail.com>
2018-02-13 13:24:05 -0500

    try it without checking remember cookie (#704)

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -17,7 +17,7 @@ export const PERMISSION_STORY_EDIT = 'story-edit';
 
 const COOKIE_USERNAME = 'mc_username';
 const SESSION_COOKIE_NAME = 'mc_session';
-const REMEMBER_COOKIE_NAME = 'mc_remember_token';
+// const REMEMBER_COOKIE_NAME = 'mc_remember_token';
 const COOKIE_DOMAIN = document.appConfig.cookieDomain;
 
 const cookiesContext = new Cookies();
@@ -37,8 +37,7 @@ export function logout() {
 }
 
 export function hasCookies() {
-  const hasUsernameCookie = (cookiesContext.get(SESSION_COOKIE_NAME) !== undefined) &&
-    (cookiesContext.get(REMEMBER_COOKIE_NAME) !== undefined);
+  const hasUsernameCookie = (cookiesContext.get(SESSION_COOKIE_NAME) !== undefined);
   return hasUsernameCookie;
 }
 
