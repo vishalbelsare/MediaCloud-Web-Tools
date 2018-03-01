b021b16761461d62c76725997eaa1805e22b1e74
Rahul B <rahulbot@gmail.com>
2018-03-01 16:19:28 -0500

    Centralize caches in apicache

--- server/views/explorer/stories.py
+++ server/views/explorer/stories.py
@@ -4,12 +4,12 @@ import flask_login
 import json
 from mediacloud.api import MediaCloud
 
-from server import app, mc
-from server.cache import cache, key_generator
+from server import app
 import server.util.csv as csv
-from server.util.request import api_error_handler
+from server.util.request import api_error_handler, arguments_required
 from server.views.explorer import prep_simple_solr_query, parse_as_sample, parse_query_with_args_and_sample_search,\
     parse_query_with_keywords, load_sample_searches
+import server.views.explorer.apicache as apicache
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 def api_explorer_story_sample():
     solr_query = parse_query_with_keywords(request.args)
  
-    story_sample_result = cached_random_stories(solr_query)
+    story_sample_result = apicache.random_story_list(solr_query, 50)
     return jsonify(story_sample_result)  
 
 
@@ -36,13 +36,8 @@ def api_explorer_demo_story_sample():
     else:
         solr_query = parse_query_with_keywords(request.args)
  
-    story_sample_result = cached_random_stories(solr_query)
+    story_sample_result = apicache.random_story_list(solr_query, 50)
     return jsonify(story_sample_result)  
-
-
-@cache.cache_on_arguments(function_key_generator=key_generator)
-def cached_random_stories(query):
-    return mc.storyList(solr_query=query, sort=mc.SORT_RANDOM)
 
 
 # if this is a sample search, we will have a search id and a query index
@@ -54,7 +49,7 @@ def explorer_stories_csv(search_id_or_query, index=None):
         search_id = int(search_id_or_query)
         if search_id >= 0:  # this is a sample query
             solr_query = parse_as_sample(search_id, index)
-            # TODO 
+            # TODO
             filename = filename  # don't have this info + current_query['q']
         # for demo users we only download 100 random stories (ie. not all matching stories)
         return _stream_story_list_csv(filename, solr_query, 100, MediaCloud.SORT_RANDOM, 1)
@@ -99,14 +94,14 @@ def _story_list_by_page(query, stories_per_page, sort, page_limit=None):
     page_count = 0
     media_cache = {}  # media_id => media object
     while True:
-        if (page_limit is not None) and (page_limit >= page_limit):
+        if (page_limit is not None) and (page_count >= page_limit):
             break
-        story_page = _cached_story_list_page(query, last_processed_stories_id, stories_per_page, sort)
+        story_page = apicache.story_list_page(query, last_processed_stories_id, stories_per_page, sort)
         for s in story_page:
             # add in media metadata to the story (from lazy cache)
             media_id = s['media_id']
             if media_id not in media_cache:
-                media = _cached_media(media_id)
+                media = apicache.media(media_id)
                 media_cache[media_id] = media
             media = media_cache[media_id]
             for k, v in media['metadata'].iteritems():
@@ -121,25 +116,13 @@ def _story_list_by_page(query, stories_per_page, sort, page_limit=None):
         page_count += 1
 
 
-@cache.cache_on_arguments(function_key_generator=key_generator)
-def _cached_media(media_id):
-    # ok to be a cross-user cache here because there aren't permissions associated
-    return mc.media(media_id)
-
-
-@cache.cache_on_arguments(function_key_generator=key_generator)
-def _cached_story_list_page(query, last_processed_stories_id, stories_per_page, sort):
-    # be user-specific in this cache to be careful about permissions on stories
-    return mc.storyList(query, last_processed_stories_id=last_processed_stories_id, rows=stories_per_page, sort=sort)
-
-
 @app.route('/api/explorer/story/count', methods=['GET'])
 @flask_login.login_required
 @api_error_handler
 def api_explorer_story_count():
     solr_query = parse_query_with_keywords(request.args)
  
-    story_count_result = cached_story_count(solr_query)
+    story_count_result = apicache.story_count(solr_query)
     return jsonify(story_count_result)  
 
 
@@ -148,20 +131,15 @@ def api_explorer_story_count():
 def api_explorer_demo_story_count():
     search_id = int(request.args['search_id']) if 'search_id' in request.args else None
     if search_id not in [None, -1]:
-        SAMPLE_SEARCHES = load_sample_searches()
-        current_search = SAMPLE_SEARCHES[search_id]['queries']
+        sample_searches = load_sample_searches()
+        current_search = sample_searches[search_id]['queries']
         solr_query = parse_query_with_args_and_sample_search(request.args, current_search)
     else:
         solr_query = parse_query_with_keywords(request.args)
 
-    story_count_result = cached_story_count(solr_query)
+    story_count_result = apicache.story_count(solr_query)
     # maybe check admin role before we run this?
     return jsonify(story_count_result)  # give them back new data, so they can update the client
-
-
-@cache.cache_on_arguments(function_key_generator=key_generator)
-def cached_story_count(query):
-    return mc.storyCount(solr_query=query)
 
 
 def stream_story_count_csv(fn, search_id_or_query_list):
@@ -181,7 +159,7 @@ def stream_story_count_csv(fn, search_id_or_query_list):
 
             for query in sample_queries:
                 solr_query = prep_simple_solr_query(query)
-                story_count = cached_story_count(solr_query)
+                story_count = apicache.story_count(solr_query)
                 query_and_story_count = {'query' : query['label'], 'count': story_count['count']}
                 story_count_results.append(query_and_story_count)
 
@@ -192,7 +170,7 @@ def stream_story_count_csv(fn, search_id_or_query_list):
             solr_query = parse_query_with_keywords(query)
             filename = fn + query['q']
 
-            story_count = cached_story_count(solr_query)
+            story_count = apicache.story_count(solr_query)
             query_and_story_count = {'query' : query['label'], 'count': story_count['count']}
             story_count_results.append(query_and_story_count)
     
