3a2e7daf52eb07e8221c9f92a736203366a27f03
Rahul B <rahulbot@gmail.com>
2018-03-05 16:01:06 -0500

    linting

--- server/views/stat.py
+++ server/views/stat.py
@@ -1,11 +1,11 @@
 import logging
 from flask import jsonify
-import flask_login
 
 from server import app, mc
 from server.util.request import api_error_handler
 
 logger = logging.getLogger(__name__)
+
 
 @app.route('/api/system-stats', methods=['GET'])
 @api_error_handler
