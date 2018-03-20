bae81bcea46218c01ce3633a27010b580c5c9e4b
cindyb <cindyloo_home at yahoo.com>
2018-03-20 09:31:12 -0400

    #968

--- src/lib/explorerUtil.js
+++ src/lib/explorerUtil.js
@@ -8,7 +8,7 @@ export const DEFAULT_SOURCES = '';
 
 export const DEFAULT_COLLECTION = 9139487;
 
-export const DEFAULT_COLLECTION_OBJECT_ARRAY = [{ id: DEFAULT_COLLECTION, tags_id: DEFAULT_COLLECTION, label: 'U.S. Top News' }];
+export const DEFAULT_COLLECTION_OBJECT_ARRAY = [{ id: DEFAULT_COLLECTION, tags_id: DEFAULT_COLLECTION, label: 'U.S. Top Online News' }];
 
 export const PICK_COLLECTION = 0;
 export const PICK_SOURCE = 1;
