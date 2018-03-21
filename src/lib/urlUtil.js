653524841d970739758fb5ed124cddd4525ce6b2
cindyb <cindyloo_home at yahoo.com>
2018-03-21 11:10:41 -0400

    #935

--- src/lib/urlUtil.js
+++ src/lib/urlUtil.js
@@ -55,5 +55,13 @@ export function urlToDashboardQuery(name, keywords, sourceIds, collectionIds, st
 }
 
 export function urlToExplorerQuery(name, keywords, sourceIds, collectionIds, startDate, endDate) {
-  return `https://explorer.mediacloud.org/#/queries/search?q=[{"label":"${name}","q":"${keywords}","color":"${getBrandDarkerColor().substr(1)}","startDate":"${startDate}","endDate":"${endDate}","sources":${JSON.stringify(sourceIds)},"collections":${JSON.stringify(collectionIds)}}]`;
+  let sources = sourceIds;
+  if (!sources || sources.length === 0) {
+    sources = '[]';
+  }
+  let collections = collectionIds;
+  if (!collections || collections.length === 0) {
+    collections = '[]';
+  }
+  return `https://explorer.mediacloud.org/#/queries/search?q=[{"label":"${name}","q":"${keywords}","color":"${getBrandDarkerColor().substr(1)}","startDate":"${startDate}","endDate":"${endDate}","sources":${sources},"collections":${collections}}]`;
 }
