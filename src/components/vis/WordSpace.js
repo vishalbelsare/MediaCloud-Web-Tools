ee53667a1e339d16d134a9bb25da31b2ffda5e02
Rebekah Bell <bellr@mit.edu>
2018-03-21 23:06:57 -0400

    basic word space implementation (no zoom)

--- src/components/vis/WordSpace.js
+++ src/components/vis/WordSpace.js
@@ -11,7 +11,7 @@ const localMessages = {
 };
 
 // TODO: width and height should always be the same; need to condense into one value
-//       (or stick square viz into a rectangular div)
+//       (or stick square svg into a rectangular div)
 const DEFAULT_WIDTH = 800;
 const DEFAULT_HEIGHT = 800;
 
@@ -179,7 +179,67 @@ function WordSpace(props) {
       .style('fill', 'none');
   }
 
-  // TODO: angle lines
+  // TODO: find more concise way to code this
+  // axis polar coord lines
+  const lineColor = '#efefef';
+  d3.select(node).append('svg:line')
+    .attr('id', 'pos-y-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(0))
+    .attr('y2', yScale(maxRadius))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'pos-x-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(maxRadius))
+    .attr('y2', yScale(0))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'neg-y-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(0))
+    .attr('y2', yScale(-maxRadius))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'neg-x-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(-maxRadius))
+    .attr('y2', yScale(0))
+    .style('stroke', lineColor);
+
+  // 45-degree lines
+  d3.select(node).append('svg:line')
+    .attr('id', 'quad-3-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(-maxRadius * Math.cos(Math.PI / 4)))
+    .attr('y2', yScale(-maxRadius * Math.sin(Math.PI / 4)))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'quad-1-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(maxRadius * Math.cos(Math.PI / 4)))
+    .attr('y2', yScale(maxRadius * Math.sin(Math.PI / 4)))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'quad-2-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(maxRadius * Math.cos(Math.PI / 4)))
+    .attr('y2', yScale(-maxRadius * Math.sin(Math.PI / 4)))
+    .style('stroke', lineColor);
+  d3.select(node).append('svg:line')
+    .attr('id', 'quad-4-axis')
+    .attr('x1', xScale(0))
+    .attr('y1', yScale(0))
+    .attr('x2', xScale(-maxRadius * Math.cos(Math.PI / 4)))
+    .attr('y2', yScale(maxRadius * Math.sin(Math.PI / 4)))
+    .style('stroke', lineColor);
 
   // Add circle at origin
   d3.select(node)
@@ -192,6 +252,22 @@ function WordSpace(props) {
 
   // Add Text Labels
   const sortedWords = words.sort((a, b) => a.tfnorm - b.tfnorm); // important to sort so z order is right
+
+  // create id map to access words with d3
+  const wordIds = {};
+  const duplicateIds = [];
+  sortedWords.forEach((word, idx) => {
+    if (word.term in wordIds) {
+      // save index of previous occurrence (lower frequency) to remove later
+      duplicateIds.push(parseInt(wordIds[word.term].substring(4), 10));
+    }
+    wordIds[word.term] = `word${idx}`;
+  });
+  // remove any duplicates encountered in list of words
+  duplicateIds.forEach((idx) => {
+    sortedWords.splice(idx, 1);
+  });
+
   d3.select(node)
     .selectAll('text')
     .data(sortedWords)
@@ -199,7 +275,7 @@ function WordSpace(props) {
       .append('text')
         .attr('text-anchor', 'middle')
         .text(d => d.term)
-        .attr('id', d => d.term)
+        .attr('id', d => wordIds[d.term])
         .attr('x', d => xScale(d[options.xProperty]))
         .attr('y', d => yScale(d[options.yProperty]))
         .attr('fill', d => blueScale(d.tfnorm))
@@ -208,42 +284,85 @@ function WordSpace(props) {
           return `${fs}px`;
         })
         .on('mouseover', (d) => {
-          // console.log(d3.select(this)); // prints undefined
+          // TODO: find a more concise way to code this?
+          // calculate angle from (x, y) coordinates
+          let currentAngle = Math.atan(Math.abs(d[options.yProperty] / d[options.xProperty])) * (180 / Math.PI);
+          const offset = Math.acos(options.cosSimThreshold) * (180 / Math.PI);
+
+          if (d[options.xProperty] > 0 && d[options.yProperty] > 0) {           // 1st quadrant
+            currentAngle = 90.0 - currentAngle - offset;
+          } else if (d[options.xProperty] > 0 && d[options.yProperty] < 0) {    // 2nd quadrant
+            currentAngle = (90.0 + currentAngle) - offset;
+          } else if (d[options.xProperty] < 0 && d[options.yProperty] < 0) {    // 3rd quadrant
+            currentAngle = (-90.0) - currentAngle - offset;
+          } else if (d[options.xProperty] < 0 && d[options.yProperty] > 0) {    // 4th quadrant
+            currentAngle = ((-90.0) + currentAngle) - offset;
+          }
+
+          // rotate and show arc tooltip
+          d3.select('#arc')
+            .attr('transform', `translate(${xScale(0)}, ${yScale(0)}) rotate(${currentAngle})`);
+          d3.select('#arc')
+            .style('opacity', 1);
+
           // highlight selected word and move to front
-          d3.select(`#${d.term}`)
+          const event = d3.event;
+          d3.select(event.target)
             .transition()
             .duration(DEFAULT_MOUSEOVER_TRANSITION_TIME)
             .attr('fill', orangeScale(d.tfnorm))
             .attr('font-size', () => {
               const fs = fontSizeComputer(d, options.fullExtent, sizeRange);
-              return `${1.5 * fs}px`;
+              return `${1.2 * fs}px`;
             })
             .attr('font-weight', 'bold');
-          d3.select(`#${d.term}`).raise();
-        })
-        .on('mouseout', (d) => {
-          // return selected word to normal
-          d3.select(`#${d.term}`)
-            .transition()
-            .duration(DEFAULT_MOUSEOVER_TRANSITION_TIME)
-            .attr('fill', blueScale(d.tfnorm))
-            .attr('font-size', () => {
-              const fs = fontSizeComputer(d, options.fullExtent, sizeRange);
-              return `${fs}px`;
-            })
-            .attr('font-weight', 'bold');
+          d3.select(event.target).raise();
 
-          // this should work, but doesn't...
-          // d3.select(node)
-          //   .selectAll('text')
-          //   .transition().duration(100)
-          //   .attr('fill', blueScale(other => other.tfnorm))
-          //   .attr('font-size', (other) => {
-          //     const fs = fontSizeComputer(other, options.fullExtent, sizeRange);
-          //     return `${fs}px`;
-          //   })
-          //   .attr('font-weight', 'normal')
-          //   .attr('pointer-events', 'auto');
+          sortedWords.forEach((word) => {
+            if (word.term !== d.term) {
+              if (d.similar.map(x => x.term).includes(word.term)) {
+                // highlight orange
+                d3.select(`#${wordIds[word.term]}`)
+                  .transition()
+                  .duration(DEFAULT_MOUSEOVER_TRANSITION_TIME)
+                  .attr('fill', orangeScale(word.tfnorm))
+                  .attr('font-size', () => {
+                    const fs = fontSizeComputer(word, options.fullExtent, sizeRange);
+                    return `${1.2 * fs}px`;
+                  })
+                  .attr('font-weight', 'bold')
+                  .attr('pointer-events', 'none');
+                d3.select(`#${wordIds[word.term]}`).raise();
+              } else {
+                // fade out to gray
+                d3.select(`#${wordIds[word.term]}`)
+                  .transition()
+                  .duration(DEFAULT_MOUSEOVER_TRANSITION_TIME)
+                  .attr('fill', '#e2e2e2')
+                  .attr('pointer-events', 'none');
+              }
+            }
+          });
+        })
+        .on('mouseout', () => {
+          // reset and hide arc tooltip
+          d3.select('#arc')
+            .style('opacity', 0)
+            .attr('transform', `translate(${xScale(0)}, ${yScale(0)}) rotate(0)`);
+
+          // return selected word to normal
+          sortedWords.forEach((word) => {
+            d3.select(`#${wordIds[word.term]}`)
+              .transition()
+              .duration(100)
+              .attr('fill', blueScale(word.tfnorm))
+              .attr('font-size', () => {
+                const fs = fontSizeComputer(word, options.fullExtent, sizeRange);
+                return `${fs}px`;
+              })
+              .attr('font-weight', 'normal')
+              .attr('pointer-events', 'auto');
+          });
         });
 
   return (
