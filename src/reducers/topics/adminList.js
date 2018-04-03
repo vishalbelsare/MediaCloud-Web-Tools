289a4c7488506aca59f312aae3727383ffcf4584
cindyb <cindyloo_home at yahoo.com>
2018-04-03 17:21:32 -0400

    #956

--- src/reducers/topics/adminList.js
+++ src/reducers/topics/adminList.js
@@ -8,13 +8,16 @@ const adminList = createAsyncReducer({
   action: FETCH_ADMIN_TOPIC_LIST,
   handleSuccess: payload => ({
     ...payload,
-    topics: payload.topics.map((t) => {
+    topics: payload.map((t) => {
       // mark the ones that are "exceeded stories" - these are noise
       const updatedTopic = { ...t };
       if (t.state === 'error') {
         if (t.message.includes('solr_seed_query returned more than') || t.message.includes('which exceeds topic max stories')) {
           updatedTopic.state = 'error (too big)';
         }
+        const mostRecentJobStatus = t.snapshots.jobStatus[0];
+        updatedTopic.inErrorSince = mostRecentJobStatus && mostRecentJobStatus.last_updated ? mostRecentJobStatus.last_updated : t.state;
+        console.log(`jobs for this topic ${t.topics_id}  ${mostRecentJobStatus}`);
       }
       return updatedTopic;
     }),
