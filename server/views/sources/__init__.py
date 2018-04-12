76d8f782e73188031a53805970c3fff8cabafa4c
Rahul B <rahulbot@gmail.com>
2018-04-12 10:50:24 -0400

    Removing ability to download starred source list

--- server/views/sources/__init__.py
+++ server/views/sources/__init__.py
@@ -1,13 +1,13 @@
-from server.auth import user_has_auth_role, ROLE_MEDIA_EDIT, user_admin_mediacloud_client
+from server.auth import user_admin_mediacloud_client
 from server.cache import cache, key_generator
-from server.util.csv import download_media_csv
 
-SOURCES_TEMPLATE_PROPS_VIEW = ['media_id', 'url','name', 'pub_country', 'pub_state', 'primary_language', 'subject_country', 'media_type', 'public_notes', 'is_monitored']
-SOURCES_TEMPLATE_PROPS_EDIT = ['media_id', 'url','name', 'pub_country', 'pub_state', 'primary_language', 'subject_country', 'media_type', 'public_notes', 'is_monitored', 'editor_notes']
+SOURCES_TEMPLATE_PROPS_EDIT = ['media_id', 'url', 'name', 'pub_country', 'pub_state', 'primary_language',
+                               'subject_country', 'media_type', 'public_notes', 'is_monitored', 'editor_notes']
 
-COLLECTIONS_TEMPLATE_PROPS_EDIT = ['media_id', 'url','name', 'pub_country', 'pub_state', 'primary_language', 'subject_country', 'media_type', 'public_notes', 'is_monitored', 'editor_notes']
+COLLECTIONS_TEMPLATE_PROPS_EDIT = ['media_id', 'url', 'name', 'pub_country', 'pub_state', 'primary_language',
+                                   'subject_country', 'media_type', 'public_notes', 'is_monitored', 'editor_notes']
 
-COLLECTIONS_TEMPLATE_METADATA_PROPS = ['pub_country', 'pub_state', 'subject_country', 'primary_language', 'media_type',]
+COLLECTIONS_TEMPLATE_METADATA_PROPS = ['pub_country', 'pub_state', 'subject_country', 'primary_language', 'media_type']
 
 # hand-made whitelist of collections to show up as "featured" on source mgr homepage and in the media picker
 FEATURED_COLLECTION_LIST = [
@@ -25,14 +25,6 @@ FEATURED_COLLECTION_LIST = [
     9360523,
     9360524
 ]
-
-
-def download_sources_csv(all_media, file_prefix):
-    if user_has_auth_role(ROLE_MEDIA_EDIT):
-        what_type_download = SOURCES_TEMPLATE_PROPS_EDIT
-    else:
-        what_type_download = SOURCES_TEMPLATE_PROPS_VIEW    # no editor_notes
-    return download_media_csv(all_media, file_prefix, what_type_download)
 
 
 @cache.cache_on_arguments(function_key_generator=key_generator)
