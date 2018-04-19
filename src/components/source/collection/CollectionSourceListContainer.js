d8497d301c5c6dc576ba4807b2e5e6a40ab6cba4
Rahul B <rahulbot@gmail.com>
2018-04-19 09:07:18 -0400

    Fix transition from main collection page to manage source page (#1023)

--- src/components/source/collection/CollectionSourceListContainer.js
+++ src/components/source/collection/CollectionSourceListContainer.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import composeAsyncContainer from '../../common/AsyncContainer';
 import { fetchCollectionSourceList } from '../../../actions/sourceActions';
 import SourceList from '../../common/SourceList';
+import { getUserRoles, hasPermissions, PERMISSION_MEDIA_EDIT } from '../../../lib/auth';
 
 const CollectionSourceListContainer = props => (
   <SourceList
@@ -19,21 +20,36 @@ CollectionSourceListContainer.propTypes = {
   downloadUrl: PropTypes.string,
   // from store
   sources: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
   fetchStatus: state.sources.collections.selected.collectionSourceList.fetchStatus,
   sources: state.sources.collections.selected.collectionSourceList.sources,
+  user: state.user,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  asyncFetch: () => {
-    dispatch(fetchCollectionSourceList(ownProps.collectionId));
+  fetchData: (collectionId, props) => {
+    dispatch(fetchCollectionSourceList(ownProps.collectionId, props));
   },
 });
 
+function mergeProps(stateProps, dispatchProps, ownProps) {
+  return Object.assign({}, stateProps, dispatchProps, ownProps, {
+    asyncFetch: () => {
+      const props = {};
+      // if it is an admin user, fetch the source list with extra details so they are there for the manage sources screen
+      if (hasPermissions(getUserRoles(stateProps.user), PERMISSION_MEDIA_EDIT)) {
+        props.details = true;
+      }
+      dispatchProps.fetchData(ownProps.collectionId, props);
+    },
+  });
+}
+
 export default
-  connect(mapStateToProps, mapDispatchToProps)(
+  connect(mapStateToProps, mapDispatchToProps, mergeProps)(
     composeAsyncContainer(
       CollectionSourceListContainer
     )
