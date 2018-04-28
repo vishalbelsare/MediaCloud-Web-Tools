882a9c31b70716233e4b4c5f175f23b700aa2da9
Rahul B <rahulbot@gmail.com>
2018-04-27 21:31:22 -0400

    Version bump and build for testing

---
+++ server/static/gen/topics/app_css.76193b8c.js
@@ -0,0 +1,2 @@
+!function(t){function r(e){if(o[e])return o[e].exports;var n=o[e]={exports:{},id:e,loaded:!1};return t[e].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var o={};return r.m=t,r.c=o,r.p="/static/gen/topics/",r(0)}({0:function(t,r,o){t.exports=o(1417)},1417:function(t,r){}});
+//# sourceMappingURL=app_css.76193b8c.js.map
--- src/topicsIndex.js
+++ src/topicsIndex.js
@@ -10,7 +10,7 @@ import initializeApp from './index';
 
 ReactGA.initialize('UA-60744513-7');
 
-setVersion('2.8.1');
+setVersion('2.9.0');
 
 setAppName(APP_TOPIC_MAPPER);
 
