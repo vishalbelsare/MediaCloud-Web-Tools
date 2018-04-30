421cbb2f4b2e68b365085ec5f6fe5d4b86436a65
Rahul B <rahulbot@gmail.com>
2018-04-30 10:53:21 -0400

    more word count sampling centralization #1054

--- server/views/sources/words.py
+++ server/views/sources/words.py
@@ -1,7 +1,7 @@
 import logging
 
 from server import mc
-from server.views import WORD_COUNT_SAMPLE_SIZE, WORD_COUNT_UI_LENGTH
+from server.views import WORD_COUNT_SAMPLE_SIZE, WORD_COUNT_UI_LENGTH, WORD_COUNT_DOWNLOAD_LENGTH
 import server.util.csv as csv
 from server.util.wordembeddings import google_news_2d
 from server.cache import cache, key_generator
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def stream_wordcount_csv(user_mc_key, filename, query):
-    response = _cached_word_count(user_mc_key, query, 500, 10000)
+    response = _cached_word_count(user_mc_key, query, WORD_COUNT_DOWNLOAD_LENGTH, WORD_COUNT_SAMPLE_SIZE)
     props = ['count', 'term', 'stem']
     return csv.stream_response(response, props, filename)
 
@@ -21,7 +21,7 @@ def word_count(user_mc_key, query, num_words=WORD_COUNT_UI_LENGTH, sample_size=W
 
 
 @cache.cache_on_arguments(function_key_generator=key_generator)
-def _cached_word_count(user_mc_key, query, num_words, sample_size):
+def _cached_word_count(user_mc_key, query, num_words, sample_size=WORD_COUNT_SAMPLE_SIZE):
     api_client = mc if user_mc_key is None else user_admin_mediacloud_client()
     word_data = api_client.wordCount('*', query, num_words=num_words, sample_size=sample_size)
     words = [w['term'] for w in word_data]
