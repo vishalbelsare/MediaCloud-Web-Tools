bbbe77dedf21849f4114fd02c6d6522bf825e55a
Rahul B <rahulbot@gmail.com>
2018-05-02 15:52:44 -0400

    Fix typos

--- src/components/common/StorySwitchoverNotice.js
+++ src/components/common/StorySwitchoverNotice.js
@@ -5,7 +5,7 @@ import { WarningNotice } from './Notice';
 const localMessages = {
   title: { id: 'errors.storySwitchover.title', defaultMessage: 'Migrating to Story-Level Searches' },
   intro: { id: 'errors.storySwitchover.intro', defaultMessage: '<b>Your current queries and topics will need to be updated after May 12 because they will produce different results.</b>' },
-  details: { id: 'errors.storySwitchover.details', defaultMessage: 'We currently do most analysis and searching at the sentence level. We\'re switching to do everything at the story level on May 12, because that is what most people tell us they want. This will also help us save space and make things faster. <a href="https://mediacloud.org/news/2018/4/12/switching-to-story-based-searching-on-may-12th" target="_new">Learn more about this chnage.</a>' },
+  details: { id: 'errors.storySwitchover.details', defaultMessage: 'We currently do most analysis and searching at the sentence level. We\'re switching to do everything at the story level on May 12, because that is what most people tell us they want. This will also help us save space and make things faster. <a href="https://mediacloud.org/news/2018/4/12/switching-to-story-based-searching-on-may-12th" target="_new">Learn more about this change.</a>' },
 };
 
 const StorySwitchoverNotice = () => (
