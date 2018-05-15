783fe5ebe25133c44a938ffdc8f50f410203e654
Rahul B <rahulbot@gmail.com>
2018-05-15 09:47:23 -0400

    Clean up source details content tab

--- src/components/source/mediaSource/SourceDetailsContainer.js
+++ src/components/source/mediaSource/SourceDetailsContainer.js
@@ -118,13 +118,13 @@ class SourceDetailsContainer extends React.Component {
               </Col>
             </Row>
             <Row>
-              <Col lg={12} md={12} sm={12}>
-                <SourceGeographyContainer source={source} />
+              <Col lg={12}>
+                <SourceTopWordsContainer source={source} />
               </Col>
             </Row>
             <Row>
-              <Col lg={12}>
-                <SourceTopWordsContainer source={source} />
+              <Col lg={12} md={12} sm={12}>
+                <SourceGeographyContainer source={source} />
               </Col>
             </Row>
           </span>
