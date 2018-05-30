7caf9aff350af5e2afd17a5304da21f274277fc2
cindyb <cindyloo_home at yahoo.com>
2018-05-30 10:30:20 -0400

    #1119

--- server/views/sources/stories_split_by_time.py
+++ server/views/sources/stories_split_by_time.py
@@ -11,7 +11,8 @@ def stream_split_stories_csv(user_mc_key, filename, item_id, which):
     response = {
         'story_splits': apicache.last_year_split_story_count(user_mc_key, [which + ":" + str(item_id)])['counts']
     }
-    clean_results = [{'date': item['date'], 'numStories': item['count']} for item in response['story_splits']]
+    trimmed_date = datetime.strptime(item['date'], mc.SENTENCE_PUBLISH_DATE_FORMAT).strftime("%Y-%m-%d")
+    clean_results = [{'date': trimmed_date, 'numStories': item['count']} for item in response['story_splits']]
     clean_results = sorted(clean_results, key=itemgetter('date'))
     props = ['date', 'numStories']
     return csv.stream_response(clean_results, props, filename)
