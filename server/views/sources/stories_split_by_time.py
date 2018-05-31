ce7e7d482659fc8057d930ac55e5a36d62108ee0
cindyb <cindyloo_home at yahoo.com>
2018-05-31 09:37:14 -0400

    #1119 common funciton - sources

--- server/views/sources/stories_split_by_time.py
+++ server/views/sources/stories_split_by_time.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 
 import server.views.sources.apicache as apicache
 import server.util.csv as csv
+from server.util.stringutil import trimSolrDate
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,7 @@ def stream_split_stories_csv(user_mc_key, filename, item_id, which):
     response = {
         'story_splits': apicache.last_year_split_story_count(user_mc_key, [which + ":" + str(item_id)])['counts']
     }
-    clean_results = [{'date': datetime.strptime(item['date'], mc.SENTENCE_PUBLISH_DATE_FORMAT).strftime("%Y-%m-%d"), 'numStories': item['count']} for item in response['story_splits']]
+    clean_results = [{'date': trimSolrDate(item['date']), 'numStories': item['count']} for item in response['story_splits']]
     clean_results = sorted(clean_results, key=itemgetter('date'))
     props = ['date', 'numStories']
     return csv.stream_response(clean_results, props, filename)
