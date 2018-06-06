ebee38a717003cb678a484cc627cf4f7f30b8ce5
Rahul B <rahulbot@gmail.com>
2018-06-06 16:22:18 -0400

    Change import stmt #1123

--- server/views/sources/geocount.py
+++ server/views/sources/geocount.py
@@ -5,7 +5,7 @@ import server.util.tags as tag_utl
 from server.util.geo import COUNTRY_GEONAMES_ID_TO_APLHA3, HIGHCHARTS_KEYS
 from server.cache import cache, key_generator
 from server.auth import user_admin_mediacloud_client
-from server.views.sources.apicache import cached_timeperiod_story_count
+import server.views.sources.apicache as apicache
 from server.views.stories import QUERY_LAST_MONTH, QUERY_ENGLISH_LANGUAGE
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 def cached_geotag_count(user_mc_key, query):
     user_mc = user_admin_mediacloud_client()
     res = user_mc.storyTagCount(query, [QUERY_LAST_MONTH, QUERY_ENGLISH_LANGUAGE], tag_sets_id=tag_utl.GEO_TAG_SET)
-    full_count = cached_timeperiod_story_count(user_mc, query, QUERY_LAST_MONTH)['count']
+    full_count = apicache.cached_timeperiod_story_count(user_mc, query, QUERY_LAST_MONTH)['count']
     res = [r for r in res if int(r['tag'].split('_')[1]) in COUNTRY_GEONAMES_ID_TO_APLHA3.keys()]
     for r in res:
         geonamesId = int(r['tag'].split('_')[1])
