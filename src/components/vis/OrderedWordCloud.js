961017809747db14c794a14102490908ba364581
Rahul B <rahulbot@gmail.com>
2018-06-18 15:48:32 -0400

    finish removing ract-faux-dom #1134

--- src/components/vis/OrderedWordCloud.js
+++ src/components/vis/OrderedWordCloud.js
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import * as d3 from 'd3';
-import ReactFauxDOM from 'react-faux-dom';
-import { injectIntl } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 import fontSizeComputer from '../../lib/visUtil';
 
 const localMessages = {
@@ -20,156 +19,178 @@ const DEFAULT_MAX_FONT_SIZE = 30;
 const DEFAULT_TEXT_COLOR = '#333333';
 const DEFAULT_LINK_COLOR = '#ff0000';
 
-class OrderedWordCloud extends React.Component {
-  listCloudLayout = (words, width, extent, sizeRange) => {
-    const canvas = document.getElementById('canvas');   // TODO: replace with a constant
-    const canvasContext2d = canvas.getContext('2d');
-    let x = 0;
-    if (typeof (words) === 'undefined') {
-      return x;
+function listCloudLayout(wordNodes, width, extent, sizeRange) {
+  const canvas = document.getElementById('canvas');   // TODO: replace with a con`stant
+  const canvasContext2d = canvas.getContext('2d');
+  let x = 0;
+  if (typeof (wordNodes) === 'undefined') {
+    return x;
+  }
+  wordNodes.attr('x', (d) => {
+    const fs = fontSizeComputer(d, extent, sizeRange);
+    canvasContext2d.font = `bold ${fs}px Lato`;    // crazy hack for IE compat, instead of simply this.getComputedTextLength()
+    const metrics = canvasContext2d.measureText(d.term);
+    const textLength = metrics.width;
+    let lastX = x;
+    if (x + textLength + 10 > width) {  // TODO: replace 10 with state property for padding
+      lastX = 0;
     }
-    words.attr('x', (d) => {
-      const fs = fontSizeComputer(d, extent, sizeRange);
-      canvasContext2d.font = `bold ${fs}px Lato`;    // crazy hack for IE compat, instead of simply this.getComputedTextLength()
-      const metrics = canvasContext2d.measureText(d.term);
-      const textLength = metrics.width;
-      let lastX = x;
-      if (x + textLength + 10 > width) {  // TODO: replace 10 with state property for padding
-        lastX = 0;
-      }
-      x = lastX + textLength + (0.3 * fs);
-      return lastX;
-    });
-    let y = -0.5 * sizeRange.max;
-    let lastAdded = 0;
-    words.attr('y', function handleY(d) { // need closure here for d3.select to work right on the element
-      if (d3.select(this).attr('x') === 0) {
-        const height = 1.5 * fontSizeComputer(d, extent, sizeRange);
-        y += height;
-        lastAdded = height;
-      }
-      return y;
-    });
-    return y + lastAdded;
+    x = lastX + textLength + (0.3 * fs);
+    return lastX;
+  });
+  let y = -0.5 * sizeRange.max;
+  let lastAdded = 0;
+  wordNodes.attr('y', (d, index, data) => { // need closure here for d3.select to work right on the element
+    const xPosition = d3.select(data[index]).attr('x');
+    if (xPosition === '0') {  // WTF does this come out as a string???!?!?!?!
+      const height = 1.5 * fontSizeComputer(d, extent, sizeRange);
+      y += height;
+      lastAdded = height;
+    }
+    return y;
+  });
+  return y + lastAdded;
+}
+
+function drawViz(wrapperElement, {
+  words, width, height, minFontSize, maxFontSize, textColor, onWordClick, linkColor, showTooltips,
+  alreadyNormalized, fullExtent, domId, intl,
+}) {
+  const { formatMessage, formatNumber } = intl;
+  const options = {
+    width: width || DEFAULT_WIDTH,
+    height: height || DEFAULT_HEIGHT,
+    maxFontSize: maxFontSize || DEFAULT_MAX_FONT_SIZE,
+    minFontSize: minFontSize || DEFAULT_MIN_FONT_SIZE,
+    textColor: textColor || DEFAULT_TEXT_COLOR,
+    linkColor: linkColor || DEFAULT_LINK_COLOR,
+    showTooltips: showTooltips || false,
+    padding: 0,
+    alreadyNormalized: alreadyNormalized || false,
+    fullExtent,
+  };
+  const data = [...words];  // so we can modify them for visualization
+  // add in tf normalization
+  const allSum = d3.sum(data, term => parseInt(term.count, 10));
+  if (!options.alreadyNormalized) {
+    data.forEach((term, idx) => { data[idx].tfnorm = term.count / allSum; });
+  }
+  // create a rollover tooltip helper
+  const body = d3.select('body');
+  let tooltipDiv = body.select('.viz-tooltip.word-cloud-tooltip');
+  if (tooltipDiv.empty()) {
+    tooltipDiv = body.append('div')
+      .attr('class', 'viz-tooltip word-cloud-tooltip')
+      .style('opacity', 0);
+  }
+  // start layout calculations
+  const node = d3.select(wrapperElement)
+    .html('')   // important to empty it out first
+    .append('svg:svg'); // then add in the SVG wrapper we will be rendering to;
+  if (options.fullExtent === undefined) {
+    options.fullExtent = d3.extent(data, d => d.tfnorm);
+  }
+  const innerWidth = options.width - (2 * options.padding);
+  const svg = node
+      .attr('height', options.height)
+      .attr('width', options.width)
+      .attr('id', domId)
+      .attr('class', 'word-cloud');
+  let y = options.height;
+  const sizeRange = { min: options.minFontSize, max: options.maxFontSize };
+  let wordNodes;
+  const wordListHeight = options.height - (2 * options.padding);
+  const wordWrapper = svg.append('g')
+      .attr('transform', `translate(${2 * options.padding},0)`);
+
+  while (y >= wordListHeight && sizeRange.max > sizeRange.min) {
+    // Create words
+    wordNodes = wordWrapper.selectAll('text')
+      .data(data.slice(0, DEFAULT_WORD_COUNT), d => d.stem)
+      .enter()
+      .append('text') // for incoming data
+        .attr('class', '')
+        .attr('fill', options.textColor)
+        .attr('font-family', 'Lato, Helvetica, sans')
+        .classed('word', true)
+        .classed('hide', d => d.display === false)
+        .classed('show', d => d.display !== false)
+      .attr('font-size', d => fontSizeComputer(d, options.fullExtent, sizeRange))
+      .text(d => d.term)
+      .attr('font-weight', 'bold')
+      .on('mouseover', (d) => {
+        const event = d3.event;
+        d3.select(event.target).attr('fill', options.linkColor)
+          .attr('cursor', 'pointer');
+        if (options.showTooltips) {
+          let tooltipHtml = formatMessage(localMessages.wordCloudStem, { stem: d.stem });
+          tooltipHtml += '<br />';
+          tooltipHtml += formatMessage(localMessages.worldCloudTerm, { term: d.term });
+          tooltipHtml += '<br />';
+          tooltipHtml += formatMessage(localMessages.wordCloudCount, { count: formatNumber(d.count) });
+          tooltipDiv.transition()
+            .duration(200)
+            .style('opacity', 0.9);
+          tooltipDiv.html(tooltipHtml)
+            .style('left', `${d3.event.pageX}px`)
+            .style('top', `${d3.event.pageY - 48}px`);
+        }
+      })
+      .on('mouseout', () => {
+        const event = d3.event;
+        d3.select(event.target).attr('fill', options.textColor)
+          .attr('cursor', 'arrow');
+        if (options.showTooltips) {
+          tooltipDiv.transition()
+            .duration(500)
+            .style('opacity', 0);
+        }
+      })
+      .on('click', (d) => {
+        const event = d3.event;
+        if ((onWordClick !== null) && (onWordClick !== undefined)) {
+          onWordClick(d, d3.select(event.target));
+        }
+        return null;
+      });
+
+    // Layout
+    y = 0;
+    const leftHeight = listCloudLayout(wordNodes, innerWidth, options.fullExtent, sizeRange);
+    y = Math.max(y, leftHeight);
+    sizeRange.max -= 1;
+  }
+  if (y && y < options.height) {
+    svg.attr('height', y);
+  }
+}
+
+
+class OrderedWordCloud extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.chartWrapperRef = React.createRef();
+  }
+
+  componentDidMount() {
+    drawViz(this.chartWrapperRef.current, this.props);
+  }
+
+  componentDidUpdate() {
+    drawViz(this.chartWrapperRef.current, this.props);
   }
 
   render() {
-    const { words, width, height, minFontSize, maxFontSize, textColor, onWordClick, linkColor, showTooltips, alreadyNormalized, fullExtent, domId } = this.props;
-    const { formatMessage, formatNumber } = this.props.intl;
+    const { words } = this.props;
     const maxWordFreq = words.length > 0 ? words[0].count : 0;
     const enoughDataToRender = (words.length > 10) && (maxWordFreq > 10);
-    const options = {
-      width: width || DEFAULT_WIDTH,
-      height: height || DEFAULT_HEIGHT,
-      maxFontSize: maxFontSize || DEFAULT_MAX_FONT_SIZE,
-      minFontSize: minFontSize || DEFAULT_MIN_FONT_SIZE,
-      textColor: textColor || DEFAULT_TEXT_COLOR,
-      linkColor: linkColor || DEFAULT_LINK_COLOR,
-      showTooltips: showTooltips || false,
-      padding: 0,
-      alreadyNormalized: alreadyNormalized || false,
-      fullExtent,
-    };
-    // add in tf normalization
-    const allSum = d3.sum(words, term => parseInt(term.count, 10));
-    if (!options.alreadyNormalized) {
-      words.forEach((term, idx) => { words[idx].tfnorm = term.count / allSum; });
-    }
-    // create a rollover tooltip helper
-    const body = d3.select('body');
-    let tooltipDiv = body.select('.viz-tooltip.word-cloud-tooltip');
-    if (tooltipDiv.empty()) {
-      tooltipDiv = body.append('div')
-        .attr('class', 'viz-tooltip word-cloud-tooltip')
-        .style('opacity', 0);
-    }
-    // start layout calculations
-    const node = ReactFauxDOM.createElement('svg');
-    if (options.fullExtent === undefined) {
-      options.fullExtent = d3.extent(words, d => d.tfnorm);
-    }
-    const innerWidth = options.width - (2 * options.padding);
-    const svg = d3.select(node)
-        .attr('height', options.height)
-        .attr('width', options.width)
-        .attr('id', domId)
-        .attr('class', 'word-cloud');
-    let y = options.height;
-    const sizeRange = { min: options.minFontSize, max: options.maxFontSize };
-    let wordNodes;
-    const wordListHeight = options.height - (2 * options.padding);
-    const wordWrapper = svg.append('g')
-        .attr('transform', `translate(${2 * options.padding},0)`);
-
-    while (y >= wordListHeight && sizeRange.max > sizeRange.min) {
-      // Create words
-      wordNodes = wordWrapper.selectAll('text')
-        .data(words.slice(0, DEFAULT_WORD_COUNT), d => d.stem)
-        .enter()
-        .append('text') // for incoming data
-          .attr('class', '')
-          .attr('fill', options.textColor)
-          .attr('font-family', 'Lato, Helvetica, sans')
-          .classed('word', true)
-          .classed('hide', d => d.display === false)
-          .classed('show', d => d.display !== false)
-        .attr('font-size', d => fontSizeComputer(d, options.fullExtent, sizeRange))
-        .text(d => d.term)
-        .attr('font-weight', 'bold')
-        .on('mouseover', (d) => {
-          const event = d3.event;
-          d3.select(event.target).attr('fill', options.linkColor)
-            .attr('cursor', 'pointer');
-          if (options.showTooltips) {
-            let tooltipHtml = formatMessage(localMessages.wordCloudStem, { stem: d.stem });
-            tooltipHtml += '<br />';
-            tooltipHtml += formatMessage(localMessages.worldCloudTerm, { term: d.term });
-            tooltipHtml += '<br />';
-            tooltipHtml += formatMessage(localMessages.wordCloudCount, { count: formatNumber(d.count) });
-            tooltipDiv.transition()
-              .duration(200)
-              .style('opacity', 0.9);
-            tooltipDiv.html(tooltipHtml)
-              .style('left', `${d3.event.pageX}px`)
-              .style('top', `${d3.event.pageY - 48}px`);
-          }
-        })
-        .on('mouseout', () => {
-          const event = d3.event;
-          d3.select(event.target).attr('fill', options.textColor)
-            .attr('cursor', 'arrow');
-          if (options.showTooltips) {
-            tooltipDiv.transition()
-              .duration(500)
-              .style('opacity', 0);
-          }
-        })
-        .on('click', (d) => {
-          const event = d3.event;
-          if ((onWordClick !== null) && (onWordClick !== undefined)) {
-            onWordClick(d, d3.select(event.target));
-          }
-          return null;
-        });
-
-      // Layout
-      y = 0;
-      const leftHeight = this.listCloudLayout(wordNodes, innerWidth, options.fullExtent, sizeRange);
-      y = Math.max(y, leftHeight);
-      sizeRange.max -= 1;
-    }
-    if (y && y < options.height) {
-      svg.attr('height', y);
-    }
-
     if (enoughDataToRender) {
       return (
-        <div className="ordered-word-cloud">
-          {node.toReact()}
-        </div>
+        <div className="ordered-word-cloud" ref={this.chartWrapperRef} />
       );
     }
-    return (<div><i>{formatMessage(localMessages.wordCloudError)}</i></div>);
+    return (<div><i><FormattedMessage {...localMessages.wordCloudError} /></i></div>);
   }
 
 }
--- src/components/vis/WordCloud.js
+++ src/components/vis/WordCloud.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import * as d3 from 'd3';
 import d3LayoutCloud from 'd3-cloud';
-import ReactFauxDOM from 'react-faux-dom';
 import { injectIntl } from 'react-intl';
 
 const DEFAULT_WORD_COUNT = 100;
@@ -20,103 +19,121 @@ const localMessages = {
   wordCloudError: { id: 'wordcloud.error', defaultMessage: 'Sorry, but there aren\'t enough words to render a useful word cloud.' },
 };
 
+function drawViz(wrapperElement, {
+  words, width, height, maxFontSize, minFontSize, textColor, showTooltips, onWordClick, linkColor, intl,
+}) {
+  const { formatMessage, formatNumber } = intl;
+  const options = {
+    width: width || DEFAULT_WIDTH,
+    height: height || DEFAULT_HEIGHT,
+    maxFontSize: maxFontSize || DEFAULT_MAX_FONT_SIZE,
+    minFontSize: minFontSize || DEFAULT_MIN_FONT_SIZE,
+    textColor: textColor || DEFAULT_TEXT_COLOR,
+    linkColor: linkColor || DEFAULT_LINK_COLOR,
+    showTooltips: showTooltips || false,
+  };
+  // create a rollover tooltip helper
+  const body = d3.select('body');
+  let tooltipDiv = body.select('.viz-tooltip.word-cloud-tooltip');
+  if (tooltipDiv.empty()) {
+    tooltipDiv = body.append('div')
+      .attr('class', 'viz-tooltip word-cloud-tooltip')
+      .style('opacity', 0);
+  }
+  // start layout
+  const node = d3.select(wrapperElement)
+    .html('')   // important to empty it out first
+    .append('svg:svg'); // then add in the SVG wrapper we will be rendering to;
+  const counts = words.map(({ count }) => count);
+  const max = d3.max(counts);
+  const slope = options.maxFontSize / Math.log(max);
+  // get list of all words and sizes
+  const wordList = words.slice(0, DEFAULT_WORD_COUNT).map(w => ({
+    text: w.term,
+    stem: w.stem,
+    size: slope * Math.log(w.count),
+    display: w.display,
+  }));
+  // create wordcloud
+  d3LayoutCloud().size([options.width, options.height])
+  .words(wordList)
+  .rotate(() => (~~(Math.random() * 1) * 90))
+  .font('Arial')
+  .fontSize(d => d.size)
+  .on('end', (wordsAsData) => {
+    // Black and white
+    // var fill = d3.scale.linear().domain([0,100]).range(['black','white']);
+    // Colors
+    node
+      .attr('width', options.width).attr('height', options.height)
+      .append('g')
+      .attr('transform', `translate(${options.width / 2},${options.height / 2})`)
+      .selectAll('text')
+      .data(wordsAsData)
+      .enter()
+        .append('text')
+          .attr('font-family', 'Lato, Helvetica, sans')
+          .attr('font-size', d => `${d.size}px`)
+          .attr('fill', options.textColor)
+          .attr('text-anchor', 'middle')
+          .attr('font-weight', 'bold')
+          .attr('transform', d => `translate(${d.x},${d.y})rotate(${d.rotate})`)
+          .text(d => d.text);
+  })
+  .start();
+  if (onWordClick !== undefined) {
+    node.selectAll('text')
+      .on('mouseover', (d, index, list) => {
+        d3.select(list[index]).attr('fill', options.linkColor)
+          .attr('cursor', 'pointer');
+        if (options.showTooltips) {
+          let tooltipHtml = formatMessage(localMessages.wordCloudStem, { stem: d.stem });
+          tooltipHtml += '<br />';
+          tooltipHtml += formatMessage(localMessages.worldCloudTerm, { term: d.term });
+          tooltipHtml += '<br />';
+          tooltipHtml += formatMessage(localMessages.wordCloudCount, { count: formatNumber(d.count) });
+          tooltipDiv.transition()
+            .duration(200)
+            .style('opacity', 0.9);
+          tooltipDiv.html(tooltipHtml)
+            .style('left', `${d3.event.pageX}px`)
+            .style('top', `${d3.event.pageY - 48}px`);
+        }
+      })
+      .on('mouseout', (d, index, list) => {
+        d3.select(list[index]).attr('fill', options.textColor)
+          .attr('cursor', 'default');
+        if (options.showTooltips) {
+          tooltipDiv.transition()
+            .duration(500)
+            .style('opacity', 0);
+        }
+      })
+      .on('click', (d) => {
+        onWordClick(d);
+      });
+  }
+}
+
 class WordCloud extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.chartWrapperRef = React.createRef();
+  }
+
+  componentDidMount() {
+    drawViz(this.chartWrapperRef.current, this.props);
+  }
+
+  componentDidUpdate() {
+    drawViz(this.chartWrapperRef.current, this.props);
+  }
+
   render() {
-    const { words, width, height, maxFontSize, minFontSize, textColor, showTooltips, onWordClick, linkColor, domId } = this.props;
-    const { formatMessage, formatNumber } = this.props.intl;
-    const options = {
-      width: width || DEFAULT_WIDTH,
-      height: height || DEFAULT_HEIGHT,
-      maxFontSize: maxFontSize || DEFAULT_MAX_FONT_SIZE,
-      minFontSize: minFontSize || DEFAULT_MIN_FONT_SIZE,
-      textColor: textColor || DEFAULT_TEXT_COLOR,
-      linkColor: linkColor || DEFAULT_LINK_COLOR,
-      showTooltips: showTooltips || false,
-    };
-    // create a rollover tooltip helper
-    const body = d3.select('body');
-    let tooltipDiv = body.select('.viz-tooltip.word-cloud-tooltip');
-    if (tooltipDiv.empty()) {
-      tooltipDiv = body.append('div')
-        .attr('class', 'viz-tooltip word-cloud-tooltip')
-        .style('opacity', 0);
-    }
-    // start layout
-    const node = ReactFauxDOM.createElement('svg');
-    const counts = words.map(({ count }) => count);
-    const max = d3.max(counts);
-    const slope = options.maxFontSize / Math.log(max);
-    // get list of all words and sizes
-    const wordList = words.slice(0, DEFAULT_WORD_COUNT).map(w => ({
-      text: w.term,
-      stem: w.stem,
-      size: slope * Math.log(w.count),
-      display: w.display,
-    }));
-    // create wordcloud
-    d3LayoutCloud().size([options.width, options.height])
-    .words(wordList)
-    .rotate(() => (~~(Math.random() * 1) * 90))
-    .font('Arial')
-    .fontSize(d => d.size)
-    .on('end', (wordsAsData) => {
-      // Black and white
-      // var fill = d3.scale.linear().domain([0,100]).range(['black','white']);
-      // Colors
-      d3.select(node)
-        .attr('width', options.width).attr('height', options.height)
-        .append('g')
-        .attr('transform', `translate(${options.width / 2},${options.height / 2})`)
-        .selectAll('text')
-        .data(wordsAsData)
-        .enter()
-          .append('text')
-            .attr('font-family', 'Lato, Helvetica, sans')
-            .attr('font-size', d => `${d.size}px`)
-            .attr('fill', options.textColor)
-            .attr('text-anchor', 'middle')
-            .attr('font-weight', 'bold')
-            .attr('transform', d => `translate(${d.x},${d.y})rotate(${d.rotate})`)
-            .text(d => d.text);
-    })
-    .start();
-    if (this.onWordClick !== undefined) {
-      node.selectAll('text')
-        .on('mouseover', (d) => {
-          d3.select(this).attr('fill', this.options.linkColor)
-            .attr('cursor', 'pointer');
-          if (options.showTooltips) {
-            let tooltipHtml = formatMessage(localMessages.wordCloudStem, { stem: d.stem });
-            tooltipHtml += '<br />';
-            tooltipHtml += formatMessage(localMessages.worldCloudTerm, { term: d.term });
-            tooltipHtml += '<br />';
-            tooltipHtml += formatMessage(localMessages.wordCloudCount, { count: formatNumber(d.count) });
-            tooltipDiv.transition()
-              .duration(200)
-              .style('opacity', 0.9);
-            tooltipDiv.html(tooltipHtml)
-              .style('left', `${d3.event.pageX}px`)
-              .style('top', `${d3.event.pageY - 48}px`);
-          }
-        })
-        .on('mouseout', () => {
-          d3.select(this).attr('fill', this.options.textColor)
-            .attr('cursor', 'default');
-          if (options.showTooltips) {
-            tooltipDiv.transition()
-              .duration(500)
-              .style('opacity', 0);
-          }
-        })
-        .on('click', (d) => {
-          onWordClick(d);
-        });
-    }
+    const { domId } = this.props;
     return (
-      <div className="editable-word-cloud" id={domId}>
-        {node.toReact()}
-      </div>
+      <div className="editable-word-cloud" ref={this.chartWrapperRef} id={domId} />
     );
   }
 
