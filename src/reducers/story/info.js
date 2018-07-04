5f2a363e6adaa4a36af334acebc5abf4b4f6d28a
cindyb <cindyloo_home at yahoo.com>
2018-07-04 16:55:48 -0400

    #845 storyinfo- is this what we want

--- src/reducers/story/info.js
+++ src/reducers/story/info.js
@@ -1,11 +1,33 @@
-import { FETCH_STORY_INFO } from '../../actions/storyActions';
+import moment from 'moment';
+import { FETCH_STORY, SELECT_STORY } from '../../actions/storyActions';
+// FETCH_STORY_INFO
 import { createAsyncReducer } from '../../lib/reduxHelpers';
+import { TAG_SET_DATE_GUESS_METHOD, TAG_SET_EXTRACTOR_VERSION, TAG_SET_GEOCODER_VERSION, TAG_SET_NYT_THEMES_VERSION }
+  from '../../lib/tagUtil';
 
-const entities = createAsyncReducer({
+// returns undefined if it is not found
+function tagWithTagSetsId(tags, tagSetsId) {
+  if (tags) {
+    const tag = tags.find(t => t.tag_sets_id === tagSetsId);
+    return (tag) ? tag.tag : undefined;
+  }
+  return undefined;
+}
+
+const info = createAsyncReducer({
   initialState: {
-    info: [],
+    id: null,
   },
-  action: FETCH_STORY_INFO,
+  action: FETCH_STORY,
+  handleSuccess: payload => ({
+    ...payload.info,
+    publishDateObj: moment(payload.publish_date).toDate(),
+    dateGuessMethod: tagWithTagSetsId(payload.story_tags, TAG_SET_DATE_GUESS_METHOD),
+    extractorVersion: tagWithTagSetsId(payload.story_tags, TAG_SET_EXTRACTOR_VERSION),
+    geocoderVersion: tagWithTagSetsId(payload.story_tags, TAG_SET_GEOCODER_VERSION),
+    nytThemesVersion: tagWithTagSetsId(payload.story_tags, TAG_SET_NYT_THEMES_VERSION),
+  }),
+  [SELECT_STORY]: payload => ({ id: payload }),
 });
 
-export default entities;
+export default info;
