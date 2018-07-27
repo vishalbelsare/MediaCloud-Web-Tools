c65a46af1ec089f9942bf4a473723d4b92e9705d
Rahul B <rahulbot@gmail.com>
2018-07-27 12:39:05 -0400

    Work on webpack4 compilation

--- config/base.config.js
+++ config/base.config.js
@@ -1,51 +1,53 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-
 const path = require('path');
 const webpack = require('webpack');
 const ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
 const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // many of the webpack directives need an absolute path
 const basedir = path.resolve(__dirname, '../');
 
+// be smart about output based on dev or not
+// const devMode = process.env.NODE_ENV !== 'production';
+
 const baseConfig = {
   entry: {
     // the entrypoint for shared CSS used across all the apps
-    app_css: [path.resolve(basedir, 'src', 'styles', 'app', 'app.scss')],
-  },
-  output: {
-    path: path.resolve(basedir, 'build', 'public'),
-    filename: '[name].[hash:8].js',
+    common_css: [path.resolve(basedir, 'src', 'styles', 'app', 'app.scss')],
   },
   plugins: [
     // needed this to get eslint working for some reason
     new webpack.LoaderOptionsPlugin({ options: {} }),
     // this writes a file for our Flask server to read (pointing at the compiled JS)
     new ManifestRevisionPlugin(
-      path.resolve(basedir, 'build', 'manifest.json'),
+      path.resolve(basedir, 'build', 'manifest.json'), // keep this path in sync with FlaskWebpack config
       { rootAssetPath: './src' } // important that this be relative, not absolute
     ),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
   ],
   module: {
     rules: [
       { // run linting check on all the javascript code before trying to compile it
         enforce: 'pre',
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'eslint-loader',
+        exclude: /node_modules/, // we don't want to check the imported packages for formatting
+        use: 'eslint-loader', // use eslint as the engine for checking linting
       },
-      { // transpile the javascript code with babel
+      { // compile all our javascript code
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /node_modules/, // we don't want to recompile the imported packages
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // run all the code through bable to transpile it down to vanilla JS
           // don't put options here; otherwise they override what is in the .babelrc
         },
       },
       { // turn all the sass into regular CSS
         test: /\.(css|scss)$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader, // tell it to compile CSS, not JS
           'css-loader',
           'sass-loader',
         ],
@@ -59,6 +61,14 @@ const devConfig = {
   mode: 'development',
   // gnerate source maps to help debug in browser
   devtool: 'source-map',
+  // where to build dev files to for webpack-serve to work
+  output: {
+    path: path.resolve(basedir, 'build', 'public'),
+    pathinfo: true,
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].js',
+    publicPath: 'http://localhost:2992/', // needed to get correct path in dev manifest file
+  },
 };
 
 const explorerConfig = {
