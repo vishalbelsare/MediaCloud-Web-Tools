11a85d328b530dbf632af9f756295c63737ee206
Rahul B <rahulbot@gmail.com>
2018-08-08 14:24:30 -0400

    change "representation" to "entities" #1220

--- src/resources/messages.js
+++ src/resources/messages.js
@@ -12,7 +12,7 @@ const messages = {
   toolsAppName: { id: 'app.tools.name', defaultMessage: 'Tools' },
   toolsAppDescription: { id: 'app.tools.description', defaultMessage: 'Support for MediaCloud, including FAQs, tools and how to use them' },
   explorerToolName: { id: 'app.explorer.name', defaultMessage: 'Explorer' },
-  explorerToolDescription: { id: 'app.explorer.description', defaultMessage: 'Get a quick overview of how your topic of interest is covered by digital news media by exploring attention, language, and representation.' },
+  explorerToolDescription: { id: 'app.explorer.description', defaultMessage: 'Get a quick overview of how your topic of interest is covered by digital news media by exploring attention, language, and entities.' },
   readGuide: { id: 'app.readGuide', defaultMessage: 'Read User Guide' },
 
   c4cmName: { id: 'c4cm.name', defaultMessage: 'MIT Center for Civic Media' },
@@ -263,7 +263,7 @@ const messages = {
   attention: { id: 'attention', defaultMessage: 'Attention' },
   storiesPerDay: { id: 'source.storiesPerDay', defaultMessage: 'Stories per Day' },
   sourceStartDate: { id: 'source.startDate', defaultMessage: 'First Story' },
-  representation: { id: 'representation', defaultMessage: 'Representation' },
+  representation: { id: 'representation', defaultMessage: 'Entities' },
   influence: { id: 'influence', defaultMessage: 'Influence' },
 
   countsVsPercentageHelp: { id: 'explorer.countsVsPercentageHelp', defaultMessage: '<p><b>Counts vs. Percentage</b><br />You can view the attention devoted to your issue in terms of absolute counts or as percentages.  Absolute counts suffer from weekly ebbs and flows, and while searching collections don\'t reflect when we add more sources.  You can switch to view percentages in order to see normalized results that you can compare across sources and collections. To generate the normalization we run each query again without the keywords.</p>' },
