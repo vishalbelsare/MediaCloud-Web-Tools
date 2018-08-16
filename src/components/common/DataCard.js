a6f4e6cb45a59fa7bca66aa3e8d35e95238af1af
Rahul B <rahulbot@gmail.com>
2018-08-16 16:17:18 -0400

    add data card left border option, to not confuse with new outline==button style

--- src/components/common/DataCard.js
+++ src/components/common/DataCard.js
@@ -6,12 +6,13 @@ import React from 'react';
  * be inside of a DataCard.
  */
 const DataCard = (props) => {
-  const { children, border, disabled, className, inline } = props;
+  const { children, border, disabled, className, inline, leftBorder } = props;
   const unborderedClass = (border === false) ? 'unbordered' : '';
   const disabledClass = (disabled === true) ? 'disabled' : '';
+  const leftBorderClass = (leftBorder === true) ? 'left-border' : '';
   const inlineClass = (inline === true) ? 'inline' : '';
   const rootClasses = `${unborderedClass} ${disabledClass}`;
-  const classes = `datacard ${rootClasses || ''} ${className || ''} ${inlineClass || ''}`;
+  const classes = `datacard ${rootClasses} ${className} ${inlineClass} ${leftBorderClass}`;
   return (
     <div className={classes}>
       {children}
@@ -23,6 +24,7 @@ DataCard.propTypes = {
   // from parent
   children: PropTypes.node.isRequired,
   border: PropTypes.bool,
+  leftBorder: PropTypes.bool,
   disabled: PropTypes.bool,
   inline: PropTypes.bool,
   className: PropTypes.string,
